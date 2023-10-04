e5c722fa66c0985418a68a04b309fb1dc8617d6b
Hirchyts Daniil <daniil.hirchyts@etu.umontpellier.fr>
2023-10-04 17:32:57 +0200

    Frontend creeer offre update

--- src/view/creeroffre.php
+++ src/view/creeroffre.php
@@ -4,30 +4,34 @@
 
             <div class="flex mt-4">
                 <label class="flex items-center">
-                    <input type="radio" class="form-radio" name="radios" value="stage" required onchange="showDistanciel()">
+                    <input type="radio" class="form-radio" name="radios" value="stage" required
+                           onchange="showDistanciel()">
                     <span class="ml-2">Stage</span>
                 </label>
                 <label class="flex items-center ml-6">
-                    <input type="radio" class="form-radio" name="radios" value="alternance" id="alternanceRadio" required onchange="showDistanciel()">
+                    <input type="radio" class="form-radio" name="radios" value="alternance" id="alternanceRadio"
+                           required onchange="showDistanciel()">
                     <span class="ml-2">Alternance</span>
                 </label>
             </div>
-
-            <label for="titre">Titre du poste</label>
-            <input type="text" placeholder="Développeur web" name="titre" id="titre" required
-                   class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light "/>
-
-            <label for="theme">Thématique</label>
-            <select name="theme" id="theme" required
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light">
-                <option disabled selected value="">Selectionne une thématique</option>
-                <option value="gestion">Gestion</option>
-                <option value="bdd">Base de donnée</option>
-                <option value="reseaux">Réseaux</option>
-                <option value="secu">Sécurité</option>
-                <option value="développement web">Développement Web</option>
-                <option value="dev app">Développement Application</option>
-            </select>
+            <div>
+                <label for="titre">Titre du poste</label>
+                <input type="text" placeholder="Développeur web" name="titre" id="titre" required
+                       class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light "/>
+            </div>
+            <div>
+                <label for="theme">Thématique</label>
+                <select name="theme" id="theme" required
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light">
+                    <option disabled selected value="">Selectionne une thématique</option>
+                    <option value="gestion">Gestion</option>
+                    <option value="bdd">Base de donnée</option>
+                    <option value="reseaux">Réseaux</option>
+                    <option value="secu">Sécurité</option>
+                    <option value="développement web">Développement Web</option>
+                    <option value="dev app">Développement Application</option>
+                </select>
+            </div>
             <div class="flex flex-row gap-4">
                 <div class="w-full">
                     <label for="nbjour">Nombre jours semaine</label>
@@ -46,14 +50,18 @@
                 </div>
             </div>
 
-            <label for="salaire">Tarif Horraire</label>
-            <input type="number" placeholder="4.05" name="salaire" id="salaire" min="4.05"  required
-                   class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light "/>
+            <div>
 
-            <label for="avantage">Avantage</label>
-            <input type="text" placeholder="Avantage" name="avantage" id="avantage" required
-                   class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light "/>
+                <label for="salaire">Tarif Horraire</label>
+                <input type="number" placeholder="4.05" name="salaire" id="salaire" min="4.05" required
+                       class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light "/>
+            </div>
 
+            <div>
+                <label for="avantage">Avantage</label>
+                <input type="text" placeholder="Avantage" name="avantage" id="avantage" required
+                       class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light "/>
+            </div>
             <div class="flex flex-row gap-4">
                 <div class="w-full">
                     <label for="dated">Date de début</label>
@@ -74,10 +82,11 @@
 
             <label for="description">Description</label>
             <textarea name="description" id="description" cols="30" rows="10" required
-                      class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light "></textarea>
+                      class="shadow-sm max-h-[100px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light "></textarea>
 
             <input type='hidden' name='action' value='createoffre'>
-            <input type="submit" value="Envoyer" class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"/>
+            <input type="submit" value="Envoyer"
+                   class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"/>
         </div>
     </form>
 </div>
@@ -85,6 +94,6 @@
     function showDistanciel() {
         var radios = document.getElementById("alternanceRadio");
         var distancielDiv = document.getElementById('distancielDiv');
-        distancielDiv.style.display = radios.checked?'block':'none'
+        distancielDiv.style.display = radios.checked ? 'block' : 'none'
     }
 </script>
