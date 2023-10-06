30fd2203b2c15637d91d5b37804e7451ec019030
Marius Brt <marius.brouty@gmail.com>
2023-10-07 01:16:21 +0200

    Link between register and pro login

--- src/view/pro_login.php
+++ src/view/pro_login.php
@@ -9,7 +9,7 @@ use app\src\model\Auth\ProLogin;
 
 <div class="w-full md:max-w-[75%] gap-4 flex flex-col">
 
-    <h1>Login</h1>
+    <h1>Pro Login</h1>
 
     <?php $form = Form::begin('', 'post') ?>
     <div class="w-full gap-4 flex flex-col">
@@ -20,6 +20,7 @@ use app\src\model\Auth\ProLogin;
         <button class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
             Submit
         </button>
+        <a href="register">Créer un compte entreprise</a>
         <?= $model->getFirstError("login"); ?>
     </div>
     <?php Form::end() ?>
--- src/view/register.php
+++ src/view/register.php
@@ -7,7 +7,7 @@ $form = new Form();
 ?>
 <div class="w-full md:max-w-[75%] gap-4 flex flex-col">
 
-    <h1>Register</h1>
+    <h1>Pro Register</h1>
 
     <?php $form = Form::begin('', 'post') ?>
     <div class="w-full gap-4 flex flex-col">
@@ -21,6 +21,7 @@ $form = new Form();
         <button class="text-white bg-zinc-700 hover:bg-zinc-800 focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-zinc-600 dark:hover:bg-zinc-700 dark:focus:ring-zinc-800">
             Submit
         </button>
+        <a href="pro_login">Connexion pro</a>
         <?= $model->getFirstError("register"); ?>
     </div>
 
