cfacd87dcd28cb1cc642ce47c5ef8a0ef066f03d
Hirchyts Daniil <daniil.hirchyts@etu.umontpellier.fr>
2023-10-12 18:28:21 +0200

    Validation offre + Archive

--- src/view/dashboard/dashboard.php
+++ src/view/dashboard/dashboard.php
@@ -1,5 +1,3 @@
-<div class="max-w-[75%]">
-    <h1>
-        Dashboard
-    </h1>
-</div>
+<?php
+?>
+
--- src/view/offres/offre.php
+++ src/view/offres/offre.php
@@ -26,12 +26,12 @@ include_once "modal-delete.php";
         </form>
         <button id="btn-danger-delete-<?= $id_offre ?>"
                 class="formAdminSupprimer  flex items-center justify-center w-7 h-7 border-[1px] border-zinc-100 duration-150 bg-zinc-50 hover:invert  rounded-full">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor"
                  class="w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round"
                       d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0l-3-3m3 3l3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"/>
             </svg>
-
         </button>
     </div>
     <div class=" rounded-[10px] relative cursor-pointer group bg-white p-4 shadow-sm hover:shadow min-w-[200px] shrink duration-150 border-2 border-zinc-200 hover:border-zinc-300">
