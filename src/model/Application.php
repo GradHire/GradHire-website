87b99c04be8e3cb37d921767fa7c46d72ee75cd0
Marius Brt <marius.brouty@gmail.com>
2023-10-13 14:47:10 +0200

    Check if user is archived when is already connected
    #59

--- src/model/Application.php
+++ src/model/Application.php
@@ -8,92 +8,92 @@ use app\src\model\Users\User;
 
 class Application
 {
-	const EVENT_BEFORE_REQUEST = 'beforeRequest';
-	const EVENT_AFTER_REQUEST = 'afterRequest';
-	public static Application $app;
-	public static string $ROOT_DIR;
-	private static ?User $user = null;
-	public string $layout = 'main';
-	public Router $router;
-	public Request $request;
-	public Response $response;
-	public ?Controller $controller = null;
-	public View $view;
-	protected array $eventListeners = [];
+    const EVENT_BEFORE_REQUEST = 'beforeRequest';
+    const EVENT_AFTER_REQUEST = 'afterRequest';
+    public static Application $app;
+    public static string $ROOT_DIR;
+    private static ?User $user = null;
+    public string $layout = 'main';
+    public Router $router;
+    public Request $request;
+    public Response $response;
+    public ?Controller $controller = null;
+    public View $view;
+    protected array $eventListeners = [];
 
-	/**
-	 * @throws ServerErrorException
-	 */
-	public function __construct($rootDir)
-	{
-		self::$ROOT_DIR = $rootDir;
-		self::$app = $this;
-		$this->request = new Request();
-		$this->response = new Response();
-		$this->router = new Router($this->request, $this->response);
-		$this->view = new View();
+    /**
+     * @throws ServerErrorException
+     */
+    public function __construct($rootDir)
+    {
+        self::$ROOT_DIR = $rootDir;
+        self::$app = $this;
+        $this->request = new Request();
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
+        $this->view = new View();
 
-		if (isset($_COOKIE["token"])) {
-			$user_token = Token::verify($_COOKIE["token"]);
-			if (!is_null($user_token)) {
-				if (!isset($_SESSION["user"]))
-					self::setUser(Auth::load_user_by_id($user_token["id"]));
-			} else
-				session_destroy();
-		} else
-			session_destroy();
-	}
+        if (isset($_COOKIE["token"])) {
+            $user_token = Token::verify($_COOKIE["token"]);
+            if (!is_null($user_token)) {
+                if (!isset($_SESSION["user"]))
+                    self::setUser(Auth::load_user_by_id($user_token["id"]));
+            } else
+                session_destroy();
+        } else
+            session_destroy();
+    }
 
-	public static function go_home(): void
-	{
-		header("Location: /");
-	}
+    public static function go_home(): void
+    {
+        header("Location: /");
+    }
 
-	public static function getUser(): User|null
-	{
-		if (!is_null(self::$user))
-			return self::$user;
-		if (self::isGuest()) return null;
-		return unserialize($_SESSION["user"]);
-	}
+    public static function getUser(): User|null
+    {
+        if (!is_null(self::$user))
+            return self::$user;
+        if (self::isGuest()) return null;
+        return unserialize($_SESSION["user"]);
+    }
 
-	public static function setUser(User|null $user): void
-	{
-		if (is_null($user)) {
-			Auth::logout();
-			return;
-		}
-		$_SESSION["user"] = serialize($user);
-		self::$user = $user;
-	}
+    public static function setUser(User|null $user): void
+    {
+        if (is_null($user) || $user->archived()) {
+            Auth::logout();
+            return;
+        }
+        $_SESSION["user"] = serialize($user);
+        self::$user = $user;
+    }
 
-	public static function isGuest(): bool
-	{
-		return !isset($_COOKIE["token"]) || !isset($_SESSION["user"]);
-	}
+    public static function isGuest(): bool
+    {
+        return !isset($_COOKIE["token"]) || !isset($_SESSION["user"]);
+    }
 
-	public function run(): void
-	{
-		$this->triggerEvent(self::EVENT_BEFORE_REQUEST);
-		try {
-			echo $this->router->resolve();
-		} catch (\Exception $e) {
-			echo $this->router->renderView('_error', [
-				'exception' => $e,
-			]);
-		}
-	}
+    public function run(): void
+    {
+        $this->triggerEvent(self::EVENT_BEFORE_REQUEST);
+        try {
+            echo $this->router->resolve();
+        } catch (\Exception $e) {
+            echo $this->router->renderView('_error', [
+                'exception' => $e,
+            ]);
+        }
+    }
 
-	public function triggerEvent($eventName): void
-	{
-		$callbacks = $this->eventListeners[$eventName] ?? [];
-		foreach ($callbacks as $callback) {
-			call_user_func($callback);
-		}
-	}
+    public function triggerEvent($eventName): void
+    {
+        $callbacks = $this->eventListeners[$eventName] ?? [];
+        foreach ($callbacks as $callback) {
+            call_user_func($callback);
+        }
+    }
 
-	public function on($eventName, $callback): void
-	{
-		$this->eventListeners[$eventName][] = $callback;
-	}
+    public function on($eventName, $callback): void
+    {
+        $this->eventListeners[$eventName][] = $callback;
+    }
 }
