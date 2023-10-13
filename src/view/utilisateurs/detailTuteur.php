7ce38a8f771f219894d7d281de64e0374cbf4449
garroc <clement.garro@etu.umontpellier.fr>
2023-10-13 16:29:41 +0200

    redirect when update on utilisateur as admin or staff

--- src/view/utilisateurs/detailTuteur.php
+++ src/view/utilisateurs/detailTuteur.php
@@ -1,5 +1,7 @@
 <?php
 /** @var $utilisateur \app\src\model\dataObject\Tuteur */
+
+use app\src\model\Application;
 use app\src\model\repository\UtilisateurRepository;
 ?>
 
@@ -11,7 +13,7 @@ use app\src\model\repository\UtilisateurRepository;
         <div class="flex flex-row gap-4">
             <?php
             echo "<span class=\"inline-flex cursor-pointer  -space-x-px overflow-hidden rounded-md border bg-white shadow-sm\">";
-            echo("<a href=\"/edit_profile/" . $utilisateur->getIdutilisateur() . "\" class=\"cursor-pointer inline-block px-4 py-2 text-sm font-medium text-zinc-700 hover:bg-zinc-50 focus:relative\">
+            echo("<a href=\"/edit_profile/" . $utilisateur->getIdutilisateur() . "?redirect=". urlencode(Application::getRedirect())."\" class=\"cursor-pointer inline-block px-4 py-2 text-sm font-medium text-zinc-700 hover:bg-zinc-50 focus:relative\">
                 <svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-5 h-5\">
                 <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10\"/>
                 </svg>
