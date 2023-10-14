970449e273d06b9376fdb607e1d9b9e961a63641
Gozzo Giovanni <giovanni.gozzo@etu.umontpellier.fr>
2023-10-14 19:27:25 +0200

    terminer design detailCandidature.php

--- src/view/candidature/detailCandidature.php
+++ src/view/candidature/detailCandidature.php
@@ -54,8 +54,10 @@ echo '<h2 class="font-bold text-lg">Candidature de
             <dd class="mt-1 text-sm leading-6 text-zinc-700 sm:col-span-2 sm:mt-0">
                 <?php
                 $filepath = "/uploads/". $candidatures->getIdoffre()."_".$candidatures->getIdutilisateur()."/cv.pdf";
-                echo "<a href='".$filepath."' download target='_blank'>📥</a>";
-                ?>
+                echo "<a href='".$filepath."' download target='_blank'>";?> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                </svg>
+                </a>
             </dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -63,8 +65,10 @@ echo '<h2 class="font-bold text-lg">Candidature de
             <dd class="mt-1 text-sm leading-6 text-zinc-700 sm:col-span-2 sm:mt-0">
                 <?php
                 $filepath = "/uploads/". $candidatures->getIdoffre()."_".$candidatures->getIdutilisateur()."/ltm.pdf";
-                echo "<a href='".$filepath."' download target='_blank'>📥</a>";
-                ?>
+                echo "<a href='".$filepath."' download target='_blank'>"; ?> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                </svg>
+                </a>
             </dd>
         </div>
     </dl>
@@ -81,9 +85,9 @@ if($candidatures->getEtatcandidature() == "on hold"){
 
     <div class="flex flex-row gap-4 w-full">
         <input type="submit" name="action" value="Accepter"
-               class="w-full text-white bg-zinc-700 hover:bg-zinc-800 focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-zinc-600 dark:hover:bg-zinc-700 dark:focus:ring-zinc-800"/>
-        <input onclick="saveForm()" type="submit" name="action" value="Refuser"
-               class=" max-w-[150px] text-white bg-zinc-700 hover:bg-zinc-800 focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-zinc-600 dark:hover:bg-zinc-700 dark:focus:ring-zinc-800"/>
+               class="w-full text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"/>
+        <input type="submit" name="action" value="Refuser"
+               class="w-full text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"/>
     </div>
 
     <?php
