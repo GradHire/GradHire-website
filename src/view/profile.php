1b4d1b650bcaa3659b751ed07b04157a23f86ad9
Hirchyts Daniil <daniil.hirchyts@etu.umontpellier.fr>
2023-10-15 23:20:26 +0200

    NavBar fix

--- src/view/profile.php
+++ src/view/profile.php
@@ -82,6 +82,15 @@ $this->title = 'Profile';
 						</a>
 					</li>
 				<?php } ?>
+                <li>
+                    <a href="/logout"
+                       class="text-zinc-700 group flex gap-x-3 rounded-md py-2 pl-2 pr-3 text-sm leading-6 font-semibold hover:bg-zinc-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 shrink-0 text-zinc-600">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                        </svg>
+                        Se déconnecter
+                    </a>
+                </li>
 			</ul>
 		</nav>
 	</aside>
