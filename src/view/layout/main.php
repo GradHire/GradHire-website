6d27cbddd2a1e7c31f24096374a379135afb6fea
Marius Brt <marius.brouty@gmail.com>
2023-11-02 19:06:48 +0100

    Fix detail offre

--- src/view/layout/main.php
+++ src/view/layout/main.php
@@ -58,8 +58,12 @@ use app\src\model\dataObject\Roles;
                     <?php if (!Application::isGuest()): ?>
                         <a href="/"
                            class="flex items-center text-xl md:text-sm font-medium text-zinc-700 hover:text-zinc-800">Accueil</a>
-                        <a href="/offres"
-                           class="flex items-center text-xl md:text-sm font-medium text-zinc-700 hover:text-zinc-800">Offres</a>
+                        <?php
+                        if (!Auth::has_role(Roles::Enterprise)):
+                            ?>
+                            <a href="/offres"
+                               class="flex items-center text-xl md:text-sm font-medium text-zinc-700 hover:text-zinc-800">Offres</a>
+                        <?php endif; ?>
                         <?php if (!Auth::has_role(Roles::Enterprise, Roles::Tutor)): ?>
                             <a href="/entreprises"
                                class="flex items-center text-xl md:text-sm font-medium text-zinc-700 hover:text-zinc-800">Entreprises</a>
