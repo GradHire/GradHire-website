c58c3a95ffb970fcc3ce584599d07ee66ff2257c
Hirchyts Daniil <daniil.hirchyts@etu.umontpellier.fr>
2023-11-16 20:53:57 +0100

    Dashboard stats

--- src/view/dashboard/dashboard.php
+++ src/view/dashboard/dashboard.php
@@ -42,15 +42,15 @@ function OffreCard($offre)
 
 
     echo <<<EOT
-<div class="h-[125px] w-full border-gray-200 border rounded-[8px] flex flex-col justify-between items-center bg-white relative px-6 py-2 mb-5">
+<div class="h-[125px] w-full border-gray-200 border rounded-[8px] flex flex-col justify-between items-center bg-white relative px-6 py-3 mb-5">
                 <div class="w-full flex flex-row items-center justify-between">
-                    <a class="font-semibold" href="/offres/{$offre['idoffre']}">Offre n°{$offre['idoffre']}</a>
+                    <a class="font-semibold hover:underline" href="/offres/{$offre['idoffre']}">Offre n°{$offre['idoffre']}</a>
                     <p class="text-zinc-400 font-light text-xs">{$date}</p>
                 </div>
                 <div class="w-full flex flex-row justify-between items-center text-xs">
                     <div class="grid grid-cols-2 max-w-[200px]">
                     <p class="font-light text-zinc-400">Nom Entreprise : </p>
-                                        <a href="/entreprises/{$offre['idutilisateur']}">{$offre['nomentreprise']}</a>
+                                        <a class="hover:underline" href="/entreprises/{$offre['idutilisateur']}">{$offre['nomentreprise']}</a>
                     <p class="font-light text-zinc-400">Sujet : </p>
                                         <p>{$offre['sujet']}</p>
 
@@ -62,13 +62,13 @@ function OffreCard($offre)
                     <p class="font-light text-zinc-400">Description : </p>
                                         <p>{$description}</p>
                     <p class="font-light text-zinc-400">Email : </p>
-                                        <p><a href="mailto:{$offre['emailentreprise']}">{$mail}</a></p>
+                                        <p><a class="hover:underline" href="mailto:{$offre['emailentreprise']}">{$mail}</a></p>
 
                     <p class="font-light text-zinc-400">Telephone : </p>
-                    <p><a href="tel:{$offre['telephoneentreprise']}">{$offre['telephoneentreprise']}</a></p>
+                    <p><a class="hover:underline" href="tel:{$offre['telephoneentreprise']}">{$offre['telephoneentreprise']}</a></p>
                     </div>
                     <div class="h-full w-[1px] bg-zinc-300"></div>
-                    <div class="rounded-[8px] shadow overflow-hidden">
+                    <div class="rounded-[8px] border overflow-hidden">
                         <iframe src="https://yandex.com/map-widget/v1/?ll=3.850089%2C43.634623&mode=search&sll=10.854186%2C49.182076&sspn=73.212891%2C44.753627&text={$offre['adresse']}&z=16.97" width="150" height="60"  allowfullscreen></iframe>
                     </div>
                 </div>
@@ -171,6 +171,7 @@ EOT;
             $svgbarChart->render();
             ?>
         </div>
+        <div class="w-full h-[1px] bg-zinc-300"></div>
         <div class="flex flex-col mt-2">
             <div class="w-full flex flex-row justify-between items-center">
                 <h2 class="text-md font-bold text-zinc-700 mb-4">Dernières offres ajoutées</h2>
@@ -182,12 +183,48 @@ EOT;
             </div>
 
             <div class="w-full flex flex-col gap-2 justify-start items-start max-h-[450px] overflow-y-scroll pr-3">
-                <div class="flex flex-row justify-start items-start w-full">
-                <?php
-                foreach ($offres as $offre) OffreCard($offre);
-                ?>
+                <div class="flex flex-row justify-start items-start w-full h-full">
+                    <div class="w-full flex flex-col justify-start items-start">
+                        <?php
+                        $counter = 0;
+                        foreach ($offres as $offre) {
+                            echo <<<EOT
+<div class="flex flex-row justify-between items-center gap-2 w-full relative">
+EOT;
+                            if ($counter != 0) {
+                                echo <<<EOT
+<span class="w-[1px] bg-zinc-300 h-[150px] mr-2"></span>
+<span class="w-6 h-6 border-zinc-300 border-b border-l rounded-bl-2xl absolute -translate-y-3 -z-[1]"></span>
+
+EOT;
+                            } else {
+                                echo <<<EOT
+<span class="w-[1px] bg-zinc-300 h-[75px] translate-y-14 mr-2"></span>
+<span class="w-6 h-6 border-zinc-300 border-t border-l rounded-tl-2xl absolute translate-y-3 -z-[1]"></span>
+EOT;
+                            }
+
+                            $counter += 1;
+                            echo <<<EOT
+<div class="min-w-[18px] min-h-[18px] max-w-[18px] flex justify-center items-center max-h-[18px] border border-zinc-300 bg-white rounded-full">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"  class="w-3 h-3 stroke-zinc-500">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+</svg>
+
+</div>
+EOT;
+
+                            OffreCard($offre);
+                            echo <<<EOT
+</div>
+EOT;
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
+
+
         </div>
     </div>
 </div>
