1bf24f01c499a89fe93faf1d5863664cc49da36c
Hirchyts Daniil <daniil.hirchyts@etu.umontpellier.fr>
2023-11-17 12:20:01 +0100

    actions devide

--- src/view/dashboard/dashboard.php
+++ src/view/dashboard/dashboard.php
@@ -15,15 +15,7 @@ $this->title = 'Dashboard';
 //print_r($data);
 //echo '</pre>';
 
-$linkOffres = new Lien('/offres', 'Offres');
-$linkUtilisateurs = new Lien('/utilisateurs', 'Gestion roles');
-$linkEntreprises = new Lien('/entreprises', 'Entreprises');
-$linkCandidatures = new Lien('/candidatures', 'Candidatures');
-$linkCreate = new Lien('/offres/create', 'Créer une offre');
-$linkTuteurs = new Lien('/ListeTuteurPro', 'Tuteurs');
-$linkImport = new Lien('/importer', 'Import');
-$linkConventions = new Lien('/conventions', 'Conventions');
-$linkExplicationSimu = new Lien('/explicationSimu', 'Simulateur');
+
 
 ?>
 <div class="w-full flex md:flex-row flex-col justify-between items-start gap-4 mx-auto pt-12 pb-24">
@@ -37,26 +29,7 @@ $linkExplicationSimu = new Lien('/explicationSimu', 'Simulateur');
             <?php } elseif ($currentTab === 'tab2') { ?>
                 <div class="flex flex-col gap-2 items-start justify-start">
                     <div class="flex flex-col w-full gap-2 items-center justify-center">
-                        <?php
-                        if (!Application::isGuest()) {
-                            if (!Auth::has_role(Roles::ChefDepartment)) echo $linkOffres->render();
-                            else echo $linkUtilisateurs->render();
-                            if (!Auth::has_role(Roles::Enterprise, Roles::Tutor, Roles::ChefDepartment)) echo $linkEntreprises->render();
-                            if (Auth::has_role(Roles::Student, Roles::Teacher, Roles::Tutor, Roles::Enterprise)) echo $linkCandidatures->render();
-                            if (Auth::has_role(Roles::Enterprise)) {
-                                echo $linkCreate->render();
-                                echo $linkTuteurs->render();
-                            }
-                            if (Auth::has_role(Roles::Manager, Roles::Staff)) {
-                                echo $linkUtilisateurs->render();
-                                echo $linkCandidatures->render();
-                                echo $linkTuteurs->render();
-                                echo $linkImport->render();
-                            }
-                            if (Auth::has_role(Roles::Student)) echo $linkExplicationSimu->render();
-                            if (Auth::has_role(Roles::Enterprise, Roles::Student, Roles::Manager, Roles::Staff)) echo $linkConventions->render();
-                        }
-                        ?>
+                        <?php require __DIR__ . '/actions.php'; ?>
                     </div>
                 </div>
             <?php } elseif ($currentTab === 'tab3') { ?>
