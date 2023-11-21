0cfb59a8225d5f0665950c9308a9e889fc2648c1
Marius Brt <marius.brouty@gmail.com>
2023-11-21 23:24:51 +0100

    Fix w3c

--- src/model/Form/fields/FormPhone.php
+++ src/model/Form/fields/FormPhone.php
@@ -5,15 +5,15 @@ namespace app\src\model\Form\fields;
 
 class FormPhone extends FormString
 {
-	public function __construct(string $name)
-	{
-		parent::__construct($name);
-		// French phone
-		$this->regex('/^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/', "Veuillez saisir un téléphone valide");
-	}
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+        // French phone
+        $this->regex('/^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/', "Veuillez saisir un téléphone valide");
+    }
 
-	protected function getType(): string
-	{
-		return "phone";
-	}
+    protected function getType(): string
+    {
+        return "tel";
+    }
 }
--- src/model/Form/fields/FormSwitch.php
+++ src/model/Form/fields/FormSwitch.php
@@ -6,54 +6,54 @@ use app\src\model\Form\rules\RuleIsBoolean;
 
 class FormSwitch extends AnyAttribute
 {
-	protected string $style = "switch";
-	private bool $red = false;
+    protected string $style = "switch";
+    private bool $red = false;
 
-	public function __construct(string $name)
-	{
-		parent::__construct($name);
-		$this->setType(new RuleIsBoolean());
-	}
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+        $this->setType(new RuleIsBoolean());
+    }
 
 
-	/**
-	 * @param bool $value
-	 * @return $this
-	 */
-	public function default($value): static
-	{
-		$this->default = $value ? "on" : "";
-		return $this;
-	}
+    /**
+     * @param bool $value
+     * @return $this
+     */
+    public function default($value): static
+    {
+        $this->default = $value ? "on" : "";
+        return $this;
+    }
 
-	function field(string $name, string $value): string
-	{
-		$value = $this->getValue($value);
-		$checked = ($value === "on" ? "checked" : '');
-		if ($this->style === "basic")
-			return <<<HTML
+    function field(string $name, string $value): string
+    {
+        $value = $this->getValue($value);
+        $checked = ($value === "on" ? "checked" : '');
+        if ($this->style === "basic")
+            return <<<HTML
 				<input type="checkbox" name="{$name}" {$checked} {$this->getParams()}/>
 			HTML;
-		$color = $this->red ? "peer-checked:bg-red-500" : "peer-checked:bg-green-500";
-		return <<<HTML
+        $color = $this->red ? "peer-checked:bg-red-500" : "peer-checked:bg-green-500";
+        return <<<HTML
 			<div class="flex flex-row gap-2">
 				<label for="AcceptConditions" class="relative h-5 w-10 cursor-pointer">
-					<input type="checkbox" id="AcceptConditions" name="{$name}" class="peer sr-only" {$checked}/>
+					<input type="checkbox" id="AcceptConditions" name="{$name}" class="peer sr-only" {$checked}>
 					<span class="absolute inset-0 rounded-full  bg-zinc-800 transition border-[1px] border-zinc-100 peer-checked:bg-green-500 [&:not(peer-checked)]:bg-red-500"></span>
 					<span class="absolute shadow inset-y-0 start-0 m-1 h-3 w-3 rounded-full bg-white border-[1px] border-zinc-100 transition-all peer-checked:start-5"></span>
 				</label>
 			</div>
 		HTML;
-	}
+    }
 
-	public function basic(): static
-	{
-		$this->style = "basic";
-		return $this;
-	}
+    public function basic(): static
+    {
+        $this->style = "basic";
+        return $this;
+    }
 
-	protected function getType(): string
-	{
-		return "checkbox";
-	}
+    protected function getType(): string
+    {
+        return "checkbox";
+    }
 }
