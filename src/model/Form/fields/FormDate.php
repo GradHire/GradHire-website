13ebf1aede5c9e8f01db53b7865a636760de5645
Marius Brt <marius.brouty@gmail.com>
2023-11-24 18:38:22 +0100

    Update FormDate.php

--- src/model/Form/fields/FormDate.php
+++ src/model/Form/fields/FormDate.php
@@ -11,37 +11,45 @@ use DateTime;
 
 class FormDate extends FormInputAttribute
 {
-	public function __construct(string $name)
-	{
-		parent::__construct($name);
-		setlocale(LC_TIME, 'fr_FR.utf8');
-		$this->setType(new RuleIsDate());
-	}
+    protected bool $useHour = false;
 
-	public function after(DateTime $date): static
-	{
-		$this->addRule(new RuleDateAfter(["after" => $date]));
-		$this->setParam("min", $date->format('Y-m-d'));
-		return $this;
-	}
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+        setlocale(LC_TIME, 'fr_FR.utf8');
+        $this->setType(new RuleIsDate());
+    }
 
-	public function before(DateTime $date): static
-	{
-		$this->addRule(new RuleDateBefore(["before" => $date]));
-		$this->setParam("max", $date->format('Y-m-d'));
-		return $this;
-	}
+    public function after(DateTime $date): static
+    {
+        $this->addRule(new RuleDateAfter(["after" => $date]));
+        $this->setParam("min", $date->format('Y-m-d'));
+        return $this;
+    }
 
-	public function between(DateTime $before, DateTime $after): static
-	{
-		$this->addRule(new RuleDateBetween(["before" => $before, "after" => $after]));
-		$this->setParam("min", $before->format('Y-m-d'));
-		$this->setParam("max", $after->format('Y-m-d'));
-		return $this;
-	}
+    public function before(DateTime $date): static
+    {
+        $this->addRule(new RuleDateBefore(["before" => $date]));
+        $this->setParam("max", $date->format('Y-m-d'));
+        return $this;
+    }
 
-	protected function getType(): string
-	{
-		return "date";
-	}
+    public function between(DateTime $before, DateTime $after): static
+    {
+        $this->addRule(new RuleDateBetween(["before" => $before, "after" => $after]));
+        $this->setParam("min", $before->format('Y-m-d'));
+        $this->setParam("max", $after->format('Y-m-d'));
+        return $this;
+    }
+
+    public function withHour(): static
+    {
+        $this->useHour = true;
+        return $this;
+    }
+
+    protected function getType(): string
+    {
+        return $this->useHour ? "datetime-local" : "date";
+    }
 }
