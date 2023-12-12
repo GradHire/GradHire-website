008b6c3ec52ce975c09463e7a3910ef055189eb4
Marius Brt <marius.brouty@gmail.com>
2023-12-12 18:54:58 +0100

    Refactor SuperviseRepo

--- src/model/repository/SuperviseRepository.php
+++ src/model/repository/SuperviseRepository.php
@@ -2,42 +2,35 @@
 
 namespace app\src\model\repository;
 
-use app\src\core\db\Database;
 use app\src\core\exception\ServerErrorException;
 use app\src\model\dataObject\Supervise;
 
 class SuperviseRepository extends AbstractRepository
 {
-    /**
-     * @throws ServerErrorException
-     */
-    public static function getByConvention(int $numConvention): Supervise|null
-    {
-        try {
-            $statement = Database::get_conn()->prepare("SELECT s.* FROM supervise s
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function getByConvention(int $numConvention): Supervise|null
+	{
+		$data = self::Fetch("SELECT s.* FROM supervise s
                 JOIN convention c ON c.idoffre = s.idoffre
-                WHERE c.numconvention=?");
-            $statement->execute([$numConvention]);
-            $data = $statement->fetch();
-            if (!$data) return null;
-            return new Supervise($data);
-        } catch (\Exception) {
-            throw new ServerErrorException();
-        }
-    }
+                WHERE c.numconvention=?", [$numConvention]);
+		if (!$data) return null;
+		return new Supervise($data);
+	}
 
-    protected function getNomTable(): string
-    {
-        return "supervise";
-    }
+	protected function getNomTable(): string
+	{
+		return "supervise";
+	}
 
-    protected function construireDepuisTableau(array $dataObjectFormatTableau): Supervise
-    {
-        return new Supervise($dataObjectFormatTableau);
-    }
+	protected function construireDepuisTableau(array $dataObjectFormatTableau): Supervise
+	{
+		return new Supervise($dataObjectFormatTableau);
+	}
 
-    protected function getNomColonnes(): array
-    {
-        return [];
-    }
+	protected function getNomColonnes(): array
+	{
+		return [];
+	}
 }
