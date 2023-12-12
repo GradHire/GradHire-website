a9dc0169ef4c13f44783e49389a9f853a0c0f349
Marius Brt <marius.brouty@gmail.com>
2023-12-12 19:08:59 +0100

    Refactor TuteurRepo

--- src/model/repository/TuteurRepository.php
+++ src/model/repository/TuteurRepository.php
@@ -2,324 +2,210 @@
 
 namespace app\src\model\repository;
 
-use app\src\core\db\Database;
 use app\src\core\exception\ServerErrorException;
 use app\src\model\dataObject\Roles;
 use app\src\model\dataObject\Tuteur;
-use PDOException;
 
 class TuteurRepository extends ProRepository
 {
-    protected static string $view = "TuteurVue";
-    protected static string $update_function = "updateTuteur";
+	protected static string $view = "TuteurVue";
+	protected static string $update_function = "updateTuteur";
 
 
-    public function role(): Roles
-    {
-        return Roles::Tutor;
-    }
+	public function role(): Roles
+	{
+		return Roles::Tutor;
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function getByIdFull($idutilisateur): ?Tuteur
-    {
-        try {
-            $sql = "SELECT * FROM " . self::$view . " WHERE idUtilisateur = :idUtilisateur";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute(['idUtilisateur' => $idutilisateur]);
-            $requete->setFetchMode(\PDO::FETCH_ASSOC);
-            $resultat = $requete->fetch();
-            if (!$resultat) {
-                return null;
-            }
-            return $this->construireDepuisTableau($resultat);
-        } catch (PDOException) {
-            throw new ServerErrorException();
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function getByIdFull($idutilisateur): ?Tuteur
+	{
+		$resultat = self::FetchAssoc("SELECT * FROM " . self::$view . " WHERE idUtilisateur = ?", [$idutilisateur]);
+		return $resultat ? $this->construireDepuisTableau($resultat) : null;
+	}
 
-    protected
-    function construireDepuisTableau(array $dataObjectFormatTableau): Tuteur
-    {
-        return new Tuteur(
-            $dataObjectFormatTableau
-        );
-    }
+	protected
+	function construireDepuisTableau(array $dataObjectFormatTableau): Tuteur
+	{
+		return new Tuteur(
+			$dataObjectFormatTableau
+		);
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function addTuteur($idUtilisateur, $idOffre, $idEtudiant): void
-    {
-        try {
-            $sql = "UPDATE Supervise SET Statut = 'validee' WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre AND idEtudiant = :idEtudiant";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute([
-                'idUtilisateur' => $idUtilisateur,
-                'idOffre' => $idOffre,
-                'idEtudiant' => $idEtudiant,
-            ]);
-            $sql = "UPDATE Staff SET role = 'tuteurprof' WHERE idUtilisateur = :idUtilisateur AND role = 'enseignant'";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute([
-                'idUtilisateur' => $idUtilisateur,
-            ]);
-            $sql = "UPDATE Postuler SET Statut = 'validee' WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute([
-                'idUtilisateur' => $idEtudiant,
-                'idOffre' => $idOffre,
-            ]);
-        } catch (PDOException) {
-            throw new ServerErrorException('erreur Update Postuler');
-        }
-        $this->refuserTuteur($idUtilisateur, $idOffre, $idEtudiant);
-        try {
-            //refuser toutes les autres candidatures
-            $sql = "UPDATE Postuler SET statut = 'refusee' WHERE idUtilisateur != :idUtilisateur AND idOffre = :idOffre";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute([
-                'idUtilisateur' => $idEtudiant,
-                'idOffre' => $idOffre,
-            ]);
-        } catch (PDOException) {
-            throw new ServerErrorException('erreur Update Postuler');
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function addTuteur($idUtilisateur, $idOffre, $idEtudiant): void
+	{
+		self::Execute("UPDATE Supervise SET Statut = 'validee' WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre AND idEtudiant = :idEtudiant", [
+			'idUtilisateur' => $idUtilisateur,
+			'idOffre' => $idOffre,
+			'idEtudiant' => $idEtudiant,
+		]);
+		self::Execute("UPDATE Staff SET role = 'tuteurprof' WHERE idUtilisateur = :idUtilisateur AND role = 'enseignant'", [
+			'idUtilisateur' => $idUtilisateur,
+		]);
+		self::Execute("UPDATE Postuler SET Statut = 'validee' WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre", [
+			'idUtilisateur' => $idEtudiant,
+			'idOffre' => $idOffre,
+		]);
+		$this->refuserTuteur($idUtilisateur, $idOffre, $idEtudiant);
+		self::Execute("UPDATE Postuler SET statut = 'refusee' WHERE idUtilisateur != :idUtilisateur AND idOffre = :idOffre", [
+			'idUtilisateur' => $idEtudiant,
+			'idOffre' => $idOffre,
+		]);
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function refuserTuteur(int $getIdutilisateur, mixed $idOffre, $idEtudiant)
-    {
-        try {
-            $sql = "UPDATE Supervise SET Statut = 'refusee' WHERE idUtilisateur!=:idUtilisateur AND idOffre = :idOffre AND idEtudiant = :idEtudiant";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute([
-                'idUtilisateur' => $getIdutilisateur,
-                'idOffre' => $idOffre,
-                'idEtudiant' => $idEtudiant
-            ]);
-        } catch (PDOException) {
-            throw new ServerErrorException('erreur refuser tuteur');
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function refuserTuteur(int $getIdutilisateur, mixed $idOffre, $idEtudiant): void
+	{
+		self::Execute("UPDATE Supervise SET Statut = 'refusee' WHERE idUtilisateur!=:idUtilisateur AND idOffre = :idOffre AND idEtudiant = :idEtudiant", [
+			'idUtilisateur' => $getIdutilisateur,
+			'idOffre' => $idOffre,
+			'idEtudiant' => $idEtudiant,
+		]);
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function getIfTuteurAlreadyExist($idUtilisateur, $idOffre, $idEtudiant): bool
-    {
-        try {
-            $sql = "SELECT * FROM Supervise s JOIN Staff st ON st.idUtilisateur = s.idUtilisateur WHERE s.idUtilisateur = :idUtilisateur AND idOffre = :idOffre AND idEtudiant = :idEtudiant AND role = 'tuteurprof' AND statut = 'validee'";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute([
-                'idUtilisateur' => $idUtilisateur,
-                'idOffre' => $idOffre,
-                'idEtudiant' => $idEtudiant
-            ]);
-            $requete->setFetchMode(\PDO::FETCH_ASSOC);
-            $resultat = $requete->fetch();
-            if ($resultat == null || $resultat["statut"] == "en attente" || $resultat["statut"] == "refusee") {
-                return false;
-            }
-            return true;
-        } catch (PDOException) {
-            throw new ServerErrorException('erreur getIfTuteurAlreadyExist');
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function getIfTuteurAlreadyExist($idUtilisateur, $idOffre, $idEtudiant): bool
+	{
+		$resultat = self::FetchAssoc("SELECT * FROM Supervise s JOIN Staff st ON st.idUtilisateur = s.idUtilisateur WHERE s.idUtilisateur = :idUtilisateur AND idOffre = :idOffre AND idEtudiant = :idEtudiant AND role = 'tuteurprof' AND statut = 'validee'", [
+			'idUtilisateur' => $idUtilisateur,
+			'idOffre' => $idOffre,
+			'idEtudiant' => $idEtudiant
+		]);
+		if ($resultat == null || $resultat["statut"] == "en attente" || $resultat["statut"] == "refusee")
+			return false;
+		return true;
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function annulerTuteur(int $iduser, int $idOffre, int $idetudiant): void
-    {
-        try {
-            //on vas d'abord verifier si il est plusieurs fois tutor
-            $sql = "SELECT COUNT(*) as 'nbFoisTuteur' FROM Supervise s JOIN Staff st on st.idUtilisateur = s.idUtilisateur WHERE s.idUtilisateur = :idUtilisateur AND statut = 'validee'";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute([
-                'idUtilisateur' => $iduser,
-            ]);
-            $requete->setFetchMode(\PDO::FETCH_ASSOC);
-            $resultat = $requete->fetch();
-            if ($resultat["nbfoistuteur"] < 1) {
-                $sql = "UPDATE Staff SET role = 'enseignant' WHERE idUtilisateur = :idUtilisateur";
-                $requete = Database::get_conn()->prepare($sql);
-                $requete->execute([
-                    'idUtilisateur' => $iduser,
-                ]);
-            }
-        } catch (PDOException) {
-            throw new ServerErrorException('erreur Insert Staff');
-        }
-        try {
-            $sql = "UPDATE Supervise SET Statut = 'en attente' WHERE idOffre = :idOffre AND idEtudiant = :idEtudiant";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute([
-                'idOffre' => $idOffre,
-                'idEtudiant' => $idetudiant
-            ]);
-        } catch (PDOException) {
-            throw new ServerErrorException('erreur annuler tuteur');
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function annulerTuteur(int $iduser, int $idOffre, int $idetudiant): void
+	{
+		$resultat = self::FetchAssoc("SELECT COUNT(*) as nbfoistuteur FROM Supervise s JOIN Staff st on st.idUtilisateur = s.idUtilisateur WHERE s.idUtilisateur = :idUtilisateur AND statut = 'validee'", [
+			'idUtilisateur' => $iduser,
+		]);
+		if ($resultat["nbfoistuteur"] < 1)
+			self::Execute("UPDATE Staff SET role = 'enseignant' WHERE idUtilisateur = :idUtilisateur", [
+				'idUtilisateur' => $iduser,
+			]);
+		self::Execute("UPDATE Supervise SET Statut = 'en attente' WHERE idOffre = :idOffre AND idEtudiant = :idEtudiant", [
+			'idOffre' => $idOffre,
+			'idEtudiant' => $idetudiant
+		]);
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function seProposerProf(int $idutilisateur, int $idOffre, int $idetudiant): void
-    {
-        try {
-            $statement = Database::get_conn()->prepare("INSERT INTO Supervise VALUES (?,?,?,'en attente',null);");
-            $statement->bindParam(1, $idutilisateur);
-            $statement->bindParam(2, $idOffre);
-            $statement->bindParam(3, $idetudiant);
-            $statement->execute();
-        } catch (\Exception $e) {
-            throw new ServerErrorException("Erreur  lors du se proposer de la convention");
-        }
-        try {
-            $statement = Database::get_conn()->prepare("UPDATE Postuler SET Statut = 'en attente tuteur entreprise' WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre");
-            $statement->bindParam(":idUtilisateur", $idetudiant);
-            $statement->bindParam(":idOffre", $idOffre);
-            $statement->execute();
-        } catch (\Exception $e) {
-            throw new ServerErrorException("Erreur lors du se proposer de la convention");
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function seProposerProf(int $idutilisateur, int $idOffre, int $idetudiant): void
+	{
+		self::Execute("INSERT INTO Supervise VALUES (?,?,?,'en attente',null);", [
+			$idutilisateur,
+			$idOffre,
+			$idetudiant
+		]);
+		self::Execute("UPDATE Postuler SET Statut = 'en attente tuteur entreprise' WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre", [
+			'idUtilisateur' => $idetudiant,
+			'idOffre' => $idOffre
+		]);
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function seDeProposerProf(int $idUtilisateur, mixed $idOffre, $idEtudiant)
-    {
-        try {
-            $statement = Database::get_conn()->prepare("DELETE FROM Supervise WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre AND idEtudiant = :idEtudiant");
-            $statement->bindParam(":idUtilisateur", $idUtilisateur);
-            $statement->bindParam(":idOffre", $idOffre);
-            $statement->bindParam(":idEtudiant", $idEtudiant);
-            $statement->execute();
-        } catch (\Exception $e) {
-            throw new ServerErrorException("Erreur lors du se de proposer de la convention");
-        }
-        try {
-            $statement = Database::get_conn()->prepare("UPDATE Postuler SET Statut = 'en attente tuteur entreprise' WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre");
-            $statement->bindParam(":idUtilisateur", $idEtudiant);
-            $statement->bindParam(":idOffre", $idOffre);
-            $statement->execute();
-        } catch (\Exception $e) {
-            throw new ServerErrorException("Erreur lors du se de proposer de la convention");
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function seDeProposerProf(int $idUtilisateur, mixed $idOffre, $idEtudiant)
+	{
+		self::Execute("DELETE FROM Supervise WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre AND idEtudiant = :idEtudiant", [
+			'idUtilisateur' => $idUtilisateur,
+			'idOffre' => $idOffre,
+			'idEtudiant' => $idEtudiant
+		]);
+		self::Execute("UPDATE Postuler SET Statut = 'en attente tuteur entreprise' WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre", [
+			'idUtilisateur' => $idEtudiant,
+			'idOffre' => $idOffre
+		]);
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function getIdOffreByIdEtuAndIdOffre(int $idetudiant, int $idoffre)
-    {
-        try {
-            $statement = Database::get_conn()->prepare("SELECT idOffre FROM Supervise WHERE idEtudiant = :idEtudiant AND idOffre = :idOffre");
-            $statement->bindParam(":idEtudiant", $idetudiant);
-            $statement->bindParam(":idOffre", $idoffre);
-            $statement->execute();
-            $statement->setFetchMode(\PDO::FETCH_ASSOC);
-            $resultat = $statement->fetch();
-            return $resultat["idOffre"];
-        } catch (\Exception $e) {
-            throw new ServerErrorException("Erreur lors du se de proposer de la convention");
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function getIdOffreByIdEtuAndIdOffre(int $idetudiant, int $idoffre)
+	{
+		$resultat = self::FetchAssoc("SELECT idOffre FROM Supervise WHERE idEtudiant = :idEtudiant AND idOffre = :idOffre", [
+			'idEtudiant' => $idetudiant,
+			'idOffre' => $idoffre
+		]);
+		return $resultat["idOffre"];
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function getNomTuteurByIdEtudiantAndIdOffre(int $idetudiant, int $idoffre): ?string
-    {
-        try {
-            $statement = Database::get_conn()->prepare("SELECT nom, prenom FROM Supervise s JOIN StaffVue st ON s.idUtilisateur = st.idUtilisateur WHERE idEtudiant = :idEtudiant AND idOffre = :idOffre");
-            $statement->bindParam(":idEtudiant", $idetudiant);
-            $statement->bindParam(":idOffre", $idoffre);
-            $statement->execute();
-            $statement->setFetchMode(\PDO::FETCH_ASSOC);
-            $resultat = $statement->fetch();
-            $prenomAndNom = $resultat["prenom"] . " " . $resultat["nom"];
-            return $prenomAndNom;
-        } catch (\Exception $e) {
-            throw new ServerErrorException("Erreur getNomTuteurByIdEtudiantAndIdOffre");
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function getNomTuteurByIdEtudiantAndIdOffre(int $idetudiant, int $idoffre): ?string
+	{
+		$resultat = self::FetchAssoc("SELECT nom, prenom FROM Supervise s JOIN StaffVue st ON s.idUtilisateur = st.idUtilisateur WHERE idEtudiant = :idEtudiant AND idOffre = :idOffre", [
+			'idEtudiant' => $idetudiant,
+			'idOffre' => $idoffre
+		]);
+		return $resultat["prenom"] . " " . $resultat["nom"];
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function getTuteurByIdEtudiantAndIdOffre(int $idetudiant, int $idoffre): ?array
-    {
-        try {
-            $statement = Database::get_conn()->prepare("SELECT * FROM Supervise s JOIN StaffVue st ON s.idUtilisateur = st.idUtilisateur WHERE idEtudiant = :idEtudiant AND idOffre = :idOffre");
-            $statement->bindParam(":idEtudiant", $idetudiant);
-            $statement->bindParam(":idOffre", $idoffre);
-            $statement->execute();
-            $statement->setFetchMode(\PDO::FETCH_ASSOC);
-            $resultat = $statement->fetch();
-            if (!$resultat) {
-                return null;
-            }
-            return $resultat;
-        } catch (\Exception $e) {
-            throw new ServerErrorException("Erreur getNomTuteurByIdEtudiantAndIdOffre");
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function getTuteurByIdEtudiantAndIdOffre(int $idetudiant, int $idoffre): ?array
+	{
+		return self::FetchAssoc("SELECT * FROM Supervise s JOIN StaffVue st ON s.idUtilisateur = st.idUtilisateur WHERE idEtudiant = :idEtudiant AND idOffre = :idOffre", [
+			'idEtudiant' => $idetudiant,
+			'idOffre' => $idoffre
+		]);
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function assigneCommeTuteurEntreprise(mixed $idUtilisateur, mixed $idOffre, mixed $idEtudiant, mixed $idTuteurEntreprise)
-    {
-        try {
-            $sql = "UPDATE Supervise SET idTuteurEntreprise = :idTuteurEntreprise WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre AND idEtudiant = :idEtudiant";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute([
-                'idUtilisateur' => $idUtilisateur,
-                'idOffre' => $idOffre,
-                'idEtudiant' => $idEtudiant,
-                'idTuteurEntreprise' => $idTuteurEntreprise
-            ]);
-        } catch (PDOException) {
-            throw new ServerErrorException('erreur assigneCommeTuteurEntreprise');
-        }
-        try {
-            $sql = "UPDATE Postuler SET statut = 'en attente responsable' WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute([
-                'idUtilisateur' => $idEtudiant,
-                'idOffre' => $idOffre,
-            ]);
-        } catch (PDOException) {
-            throw new ServerErrorException('erreur assigneCommeTuteurEntreprise');
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function assigneCommeTuteurEntreprise(mixed $idUtilisateur, mixed $idOffre, mixed $idEtudiant, mixed $idTuteurEntreprise): void
+	{
+		self::Execute("UPDATE Supervise SET idTuteurEntreprise = :idTuteurEntreprise WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre AND idEtudiant = :idEtudiant", [
+			'idUtilisateur' => $idUtilisateur,
+			'idOffre' => $idOffre,
+			'idEtudiant' => $idEtudiant,
+			'idTuteurEntreprise' => $idTuteurEntreprise
+		]);
+		self::Execute("UPDATE Postuler SET statut = 'en attente responsable' WHERE idUtilisateur = :idUtilisateur AND idOffre = :idOffre", [
+			'idUtilisateur' => $idEtudiant,
+			'idOffre' => $idOffre,
+		]);
+	}
 
-    protected
-    function getNomColonnes(): array
-    {
-        return [
-            "idutilisateur",
-            "email",
-            "nom",
-            "numtelephone",
-            "bio",
-            "archiver",
-            "prenom",
-            "fonction",
-            "identreprise"
-        ];
-    }
+	protected
+	function getNomColonnes(): array
+	{
+		return [
+			"idutilisateur",
+			"email",
+			"nom",
+			"numtelephone",
+			"bio",
+			"archiver",
+			"prenom",
+			"fonction",
+			"identreprise"
+		];
+	}
 
-    protected
-    function getNomTable(): string
-    {
-        return "TuteurVue";
-    }
+	protected
+	function getNomTable(): string
+	{
+		return "TuteurVue";
+	}
 }
