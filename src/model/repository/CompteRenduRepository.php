430c7d380a9b4fb0182ae344ee7ebcaf8c6b9d72
Marius Brt <marius.brouty@gmail.com>
2023-12-12 12:20:07 +0100

    Refactor CompteRenduRepo

--- src/model/repository/CompteRenduRepository.php
+++ src/model/repository/CompteRenduRepository.php
@@ -2,275 +2,229 @@
 
 namespace app\src\model\repository;
 
-use app\src\core\db\Database;
 use app\src\core\exception\ServerErrorException;
-use app\src\core\lib\StackTrace;
-use app\src\model\dataObject\AbstractDataObject;
 use app\src\model\dataObject\CompteRendu;
 
 class CompteRenduRepository extends AbstractRepository
 {
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function checkIfAlreadyCompteRendu(int $idtuteur, int $idetudiant, int $numconvention): bool
+	{
+		return self::Fetch(
+				"SELECT idcompterendu FROM compterendus WHERE idtuteur = :idtuteur AND idetudiant = :idetudiant AND numconvention = :numconvention",
+				['idtuteur' => $idtuteur, 'idetudiant' => $idetudiant, 'numconvention' => $numconvention]
+			) !== null;
+	}
 
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function getCommentaires(mixed $numConvention, int $studentId): ?array
+	{
+		return self::FetchAll(
+			"SELECT * FROM compterendus WHERE numconvention = :numconvention AND idetudiant = :idetudiant",
+			['numconvention' => $numConvention, 'idetudiant' => $studentId]
+		);
+	}
 
-    public static function checkIfAlreadyCompteRendu(int $idtuteur, int $idetudiant, int $numconvention): bool
-    {
-        $sql = "SELECT idcompterendu FROM compterendus WHERE idtuteur = :idtuteur AND idetudiant = :idetudiant AND numconvention = :numconvention";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'idtuteur' => $idtuteur,
-            'idetudiant' => $idetudiant,
-            'numconvention' => $numconvention
-        ]);
-        $result = $requete->fetch();
-        if ($result) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function checkIfCompteRenduExist(mixed $numconvention): bool
+	{
+		return self::Fetch("SELECT numconvention FROM compterendus WHERE numconvention = :numconvention", ['numconvention' => $numconvention]) !== null;
+	}
 
-    public static function getCommentaires(mixed $numConvention, int $studentId)
-    {
-        $sql = "SELECT * FROM compterendus WHERE numconvention = :numconvention AND idetudiant = :idetudiant";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $numConvention,
-            'idetudiant' => $studentId
-        ]);
-        $result = $requete->fetchAll();
-        return $result;
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function updateCompteRenduProf(mixed $numconvention, mixed $compteRendu): void
+	{
+		self::Execute(
+			"UPDATE compterendus SET commentaireprof = :commentaireprof WHERE numconvention = :numconvention",
+			['numconvention' => $numconvention, 'commentaireprof' => $compteRendu]
+		);
+	}
 
-    /**
-     * @throws ServerErrorException
-     */
-    public static function checkIfCompteRenduExist(mixed $numconvention)
-    {
-        try {
-        $sql = "SELECT numconvention FROM compterendus WHERE numconvention = :numconvention";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $numconvention
-        ]);
-        $result = $requete->fetch();
-        if ($result) {
-            return true;
-        } else {
-            return false;
-        } } catch (\Exception $e) {
-            StackTrace::print($e);
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function updateCompteRenduEntreprise(mixed $numconvention, mixed $compteRendu): void
+	{
+		self::Execute(
+			"UPDATE compterendus SET commentaireentreprise = :commentaireentreprise WHERE numconvention = :numconvention",
+			['numconvention' => $numconvention, 'commentaireentreprise' => $compteRendu]
+		);
+	}
 
-    public static function updateCompteRenduProf(mixed $numconvention, mixed $compteRendu)
-    {
-        $sql = "UPDATE compterendus SET commentaireprof = :commentaireprof WHERE numconvention = :numconvention";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $numconvention,
-            'commentaireprof' => $compteRendu
-        ]);
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function createCompteRenduAsProf(mixed $idtuteurprof, mixed $idtuteurentreprise, mixed $idetudiant, mixed $numconvention, mixed $compteRendu): void
+	{
+		self::Execute(
+			"INSERT INTO compterendus (idtuteurprof, idtuteurentreprise, idetudiant, numconvention, commentaireprof) VALUES (:idtuteurprof, :idtuteurentreprise, :idetudiant, :numconvention, :commentaireprof)",
+			[
+				'idtuteurprof' => $idtuteurprof,
+				'idtuteurentreprise' => $idtuteurentreprise,
+				'idetudiant' => $idetudiant,
+				'numconvention' => $numconvention,
+				'commentaireprof' => $compteRendu
+			]
+		);
+	}
 
-    public static function updateCompteRenduEntreprise(mixed $numconvention, mixed $compteRendu)
-    {
-        $sql = "UPDATE compterendus SET commentaireentreprise = :commentaireentreprise WHERE numconvention = :numconvention";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $numconvention,
-            'commentaireentreprise' => $compteRendu
-        ]);
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function createCompteRenduAsEntreprise(mixed $idtuteurprof, mixed $idtuteurentreprise, mixed $idetudiant, mixed $numconvention, mixed $compteRendu): void
+	{
+		self::Execute(
+			"INSERT INTO compterendus (idtuteurprof, idtuteurentreprise, idetudiant, numconvention, commentaireentreprise) VALUES (:idtuteurprof, :idtuteurentreprise, :idetudiant, :numconvention, :commentaireentreprise)",
+			[
+				'idtuteurprof' => $idtuteurprof,
+				'idtuteurentreprise' => $idtuteurentreprise,
+				'idetudiant' => $idetudiant,
+				'numconvention' => $numconvention,
+				'commentaireentreprise' => $compteRendu
+			]
+		);
+	}
 
-    public static function createCompteRenduAsProf(mixed $idtuteurprof, mixed $idtuteurentreprise, mixed $idetudiant, mixed $numconvention, mixed $compteRendu)
-    {
-        try {
-        $sql = "INSERT INTO compterendus (idtuteurprof, idtuteurentreprise, idetudiant, numconvention, commentaireprof) VALUES (:idtuteurprof, :idtuteurentreprise, :idetudiant, :numconvention, :commentaireprof)";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'idtuteurprof' => $idtuteurprof,
-            'idtuteurentreprise' => $idtuteurentreprise,
-            'idetudiant' => $idetudiant,
-            'numconvention' => $numconvention,
-            'commentaireprof' => $compteRendu
-        ]);
-        } catch (\Exception $e) {
-            StackTrace::print($e);
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function getIfImOneOfTheTutors(mixed $numconvention, mixed $idtuteur): ?array
+	{
+		return self::Fetch(
+			"SELECT * FROM compterendus WHERE numconvention = :numconvention AND (idtuteurprof = :idtuteur OR idtuteurentreprise = :idtuteur)",
+			['numconvention' => $numconvention, 'idtuteur' => $idtuteur]
+		);
+	}
 
-    public static function createCompteRenduAsEntreprise(mixed $idtuteurprof, mixed $idtuteurentreprise, mixed $idetudiant, mixed $numconvention, mixed $compteRendu)
-    {
-        try {
-            $sql = "INSERT INTO compterendus (idtuteurprof, idtuteurentreprise, idetudiant, numconvention, commentaireentreprise) VALUES (:idtuteurprof, :idtuteurentreprise, :idetudiant, :numconvention, :commentaireentreprise)";
-            $requete = Database::get_conn()->prepare($sql);
-            $requete->execute([
-                'idtuteurprof' => $idtuteurprof,
-                'idtuteurentreprise' => $idtuteurentreprise,
-                'idetudiant' => $idetudiant,
-                'numconvention' => $numconvention,
-                'commentaireentreprise' => $compteRendu
-            ]);
-        }catch (\Exception $e) {
-            StackTrace::print($e);
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function checkIfCompteRenduProfExist(int $getNumConvention): bool
+	{
+		return (bool)self::Fetch("SELECT commentaireprof FROM compterendus WHERE numconvention = :numconvention", ['numconvention' => $getNumConvention]);
+	}
 
-    public static function getIfImOneOfTheTutors(mixed $numconvention, mixed $idtuteur)
-    {
-        $sql = "SELECT * FROM compterendus WHERE numconvention = :numconvention AND (idtuteurprof = :idtuteur OR idtuteurentreprise = :idtuteur)";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $numconvention,
-            'idtuteur' => $idtuteur
-        ]);
-        $result = $requete->fetch();
-        return $result;
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function checkIfCompteRenduEntrepriseExist(int $getNumConvention): bool
+	{
+		return (bool)self::Fetch("SELECT commentaireentreprise FROM compterendus WHERE numconvention = :numconvention", ['numconvention' => $getNumConvention]);
+	}
 
-    public static function checkIfCompteRenduProfExist(int $getNumConvention)
-    {
-        $sql = "SELECT commentaireprof FROM compterendus WHERE numconvention = :numconvention";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $getNumConvention
-        ]);
-        $result = $requete->fetch();
-        if ($result) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function checkIfCompteRenduSoutenanceProfExist(int $getNumConvention): bool
+	{
+		return (bool)self::Fetch("SELECT commentairesoutenanceprof FROM compterendus WHERE numconvention = :numconvention", ['numconvention' => $getNumConvention]);
+	}
 
-    public static function checkIfCompteRenduEntrepriseExist(int $getNumConvention)
-    {
-        $sql = "SELECT commentaireentreprise FROM compterendus WHERE numconvention = :numconvention";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $getNumConvention
-        ]);
-        $result = $requete->fetch();
-        if($result['commentaireentreprise'] != null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function checkIfCompteRenduSoutenanceEntrepriseExist(int $getNumConvention): bool
+	{
+		return (bool)self::Fetch("SELECT commentairesoutenanceentreprise FROM compterendus WHERE numconvention = :numconvention", ['numconvention' => $getNumConvention]);
+	}
 
-    public static function checkIfCompteRenduSoutenanceProfExist(int $getNumConvention)
-    {
-        $sql = "SELECT commentairesoutenanceprof FROM compterendus WHERE numconvention = :numconvention";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $getNumConvention
-        ]);
-        $result = $requete->fetch();
-        if ($result['commentairesoutenanceprof'] != null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function checkIfCompteRenduSoutenanceExist(mixed $numconvention): bool
+	{
+		return (bool)self::Fetch(
+			"SELECT commentairesoutenanceprof, commentairesoutenanceentreprise FROM compterendus WHERE numconvention = :numconvention",
+			['numconvention' => $numconvention]
+		);
+	}
 
-    public static function checkIfCompteRenduSoutenanceEntrepriseExist(int $getNumConvention)
-    {
-        $sql = "SELECT commentairesoutenanceentreprise FROM compterendus WHERE numconvention = :numconvention";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $getNumConvention
-        ]);
-        $result = $requete->fetch();
-        if ($result['commentairesoutenanceentreprise'] != null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function updateCompteRenduSoutenanceProf(mixed $numconvention, mixed $compteRenduSoutenance): void
+	{
+		self::Execute(
+			"UPDATE compterendus SET commentairesoutenanceprof = :commentairesoutenanceprof WHERE numconvention = :numconvention",
+			['numconvention' => $numconvention, 'commentairesoutenanceprof' => $compteRenduSoutenance]
+		);
+	}
 
-    public static function checkIfCompteRenduSoutenanceExist(mixed $numconvention)
-    {
-        $sql = "SELECT commentairesoutenanceprof, commentairesoutenanceentreprise FROM compterendus WHERE numconvention = :numconvention";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $numconvention
-        ]);
-        $result = $requete->fetch();
-        if ($result['commentairesoutenanceprof'] != null && $result['commentairesoutenanceentreprise'] != null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function createCompteRenduSoutenanceAsProf(mixed $idtuteurprof, mixed $idtuteurentreprise, mixed $idetudiant, mixed $numconvention, mixed $compteRenduSoutenance): void
+	{
+		self::Execute(
+			"INSERT INTO compterendus (idtuteurprof, idtuteurentreprise, idetudiant, numconvention, commentairesoutenanceprof) VALUES (:idtuteurprof, :idtuteurentreprise, :idetudiant, :numconvention, :commentairesoutenanceprof)",
+			[
+				'idtuteurprof' => $idtuteurprof,
+				'idtuteurentreprise' => $idtuteurentreprise,
+				'idetudiant' => $idetudiant,
+				'numconvention' => $numconvention,
+				'commentairesoutenanceprof' => $compteRenduSoutenance
+			]
+		);
+	}
 
-    public static function updateCompteRenduSoutenanceProf(mixed $numconvention, mixed $compteRenduSoutenance)
-    {
-        $sql = "UPDATE compterendus SET commentairesoutenanceprof = :commentairesoutenanceprof WHERE numconvention = :numconvention";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $numconvention,
-            'commentairesoutenanceprof' => $compteRenduSoutenance
-        ]);
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function updateCompteRenduSoutenanceEntreprise(mixed $numconvention, mixed $compteRenduSoutenance): void
+	{
+		self::Execute(
+			"UPDATE compterendus SET commentairesoutenanceentreprise = :commentairesoutenanceentreprise WHERE numconvention = :numconvention",
+			['numconvention' => $numconvention, 'commentairesoutenanceentreprise' => $compteRenduSoutenance]
+		);
+	}
 
-    public static function createCompteRenduSoutenanceAsProf(mixed $idtuteurprof, mixed $idtuteurentreprise, mixed $idetudiant, mixed $numconvention, mixed $compteRenduSoutenance)
-    {
-        $sql = "INSERT INTO compterendus (idtuteurprof, idtuteurentreprise, idetudiant, numconvention, commentairesoutenanceprof) VALUES (:idtuteurprof, :idtuteurentreprise, :idetudiant, :numconvention, :commentairesoutenanceprof)";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'idtuteurprof' => $idtuteurprof,
-            'idtuteurentreprise' => $idtuteurentreprise,
-            'idetudiant' => $idetudiant,
-            'numconvention' => $numconvention,
-            'commentairesoutenanceprof' => $compteRenduSoutenance
-        ]);
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function createCompteRenduSoutenanceAsEntreprise(mixed $idtuteurprof, mixed $idtuteurentreprise, mixed $idetudiant, mixed $numconvention, mixed $compteRenduSoutenance): void
+	{
+		self::Execute(
+			"INSERT INTO compterendus (idtuteurprof, idtuteurentreprise, idetudiant, numconvention, commentairesoutenanceentreprise) VALUES (:idtuteurprof, :idtuteurentreprise, :idetudiant, :numconvention, :commentairesoutenanceentreprise)",
+			[
+				'idtuteurprof' => $idtuteurprof,
+				'idtuteurentreprise' => $idtuteurentreprise,
+				'idetudiant' => $idetudiant,
+				'numconvention' => $numconvention,
+				'commentairesoutenanceentreprise' => $compteRenduSoutenance
+			]
+		);
+	}
 
-    public static function updateCompteRenduSoutenanceEntreprise(mixed $numconvention, mixed $compteRenduSoutenance)
-    {
-        $sql = "UPDATE compterendus SET commentairesoutenanceentreprise = :commentairesoutenanceentreprise WHERE numconvention = :numconvention";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'numconvention' => $numconvention,
-            'commentairesoutenanceentreprise' => $compteRenduSoutenance
-        ]);
-    }
+	/**
+	 * @throws \Exception
+	 */
+	protected function construireDepuisTableau(array $dataObjectFormatTableau): CompteRendu
+	{
+		return new CompteRendu($dataObjectFormatTableau);
+	}
 
-    public static function createCompteRenduSoutenanceAsEntreprise(mixed $idtuteurprof, mixed $idtuteurentreprise, mixed $idetudiant, mixed $numconvention, mixed $compteRenduSoutenance)
-    {
-        $sql = "INSERT INTO compterendus (idtuteurprof, idtuteurentreprise, idetudiant, numconvention, commentairesoutenanceentreprise) VALUES (:idtuteurprof, :idtuteurentreprise, :idetudiant, :numconvention, :commentairesoutenanceentreprise)";
-        $requete = Database::get_conn()->prepare($sql);
-        $requete->execute([
-            'idtuteurprof' => $idtuteurprof,
-            'idtuteurentreprise' => $idtuteurentreprise,
-            'idetudiant' => $idetudiant,
-            'numconvention' => $numconvention,
-            'commentairesoutenanceentreprise' => $compteRenduSoutenance
-        ]);
-    }
+	protected function getNomColonnes(): array
+	{
+		return [
+			"numconvention",
+			"idetudiant",
+			"idtuteurprof",
+			"commentaireprof",
+			"idtuteurentreprise",
+			"commentaireentreprise",
+		];
+	}
 
-    /**
-     * @throws \Exception
-     */
-    protected function construireDepuisTableau(array $dataObjectFormatTableau): CompteRendu
-    {
-        return new CompteRendu($dataObjectFormatTableau);
-    }
-
-    protected function getNomColonnes(): array
-    {
-        return [
-            "numconvention",
-            "idetudiant",
-            "idtuteurprof",
-            "commentaireprof",
-            "idtuteurentreprise",
-            "commentaireentreprise",
-        ];
-    }
-
-    protected function getNomTable(): string
-    {
-        return "compterendus";
-    }
+	protected function getNomTable(): string
+	{
+		return "compterendus";
+	}
 }
