f9a4a4fdaac44046b0f944c87aff8d5d5e506862
Marius Brt <marius.brouty@gmail.com>
2023-12-12 19:27:27 +0100

    Refactor

--- src/model/Application.php
+++ src/model/Application.php
@@ -8,114 +8,111 @@ use app\src\model\repository\UtilisateurRepository;
 
 class Application
 {
-    const EVENT_BEFORE_REQUEST = 'beforeRequest';
-    const EVENT_AFTER_REQUEST = 'afterRequest';
-    public static Application $app;
-    public static string $ROOT_DIR;
-    private static ?UtilisateurRepository $user = null;
-    public string $layout = 'main';
-    public Router $router;
-    public Request $request;
-    public Response $response;
-    public ?AbstractController $controller = null;
-    public View $view;
-    protected array $eventListeners = [];
+	const EVENT_BEFORE_REQUEST = 'beforeRequest';
+	const EVENT_AFTER_REQUEST = 'afterRequest';
+	public static Application $app;
+	public static string $ROOT_DIR;
+	private static ?UtilisateurRepository $user = null;
+	public string $layout = 'main';
+	public Router $router;
+	public Request $request;
+	public Response $response;
+	public ?AbstractController $controller = null;
+	public View $view;
+	protected array $eventListeners = [];
 
-    /**
-     * @throws ServerErrorException
-     */
-    public function __construct($rootDir)
-    {
-        self::$ROOT_DIR = $rootDir;
-        self::$app = $this;
-        $this->request = new Request();
-        $this->response = new Response();
-        $this->router = new Router($this->request, $this->response);
-        $this->view = new View();
+	/**
+	 * @throws ServerErrorException
+	 */
+	public function __construct($rootDir)
+	{
+		self::$ROOT_DIR = $rootDir;
+		self::$app = $this;
+		$this->request = new Request();
+		$this->response = new Response();
+		$this->router = new Router($this->request, $this->response);
+		$this->view = new View();
 
-        if (isset($_COOKIE["token"])) {
-            $user_token = Token::verify($_COOKIE["token"]);
-            if (!is_null($user_token)) {
-                if (!isset($_SESSION["user"]))
-                    self::setUser(Auth::load_user_by_id($user_token["id"]));
-            } else
-                session_destroy();
-        } else
-            session_destroy();
-    }
+		if (isset($_COOKIE["token"])) {
+			$user_token = Token::verify($_COOKIE["token"]);
+			if (!is_null($user_token)) {
+				if (!isset($_SESSION["user"]))
+					self::setUser(Auth::load_user_by_id($user_token["id"]));
+			} else
+				session_destroy();
+		} else
+			session_destroy();
+	}
 
-    public static function go_home(): void
-    {
-        header("Location: /");
-    }
+	public static function go_home(): void
+	{
+		header("Location: /");
+	}
 
-    public static function getUser(): UtilisateurRepository|null
-    {
-        if (!is_null(self::$user)) {
-            return self::$user;
-        }
-        if (self::isGuest()) {
-            return null;
-        }
-        return unserialize($_SESSION["user"]);
-    }
+	public static function getUser(): UtilisateurRepository|null
+	{
+		if (!is_null(self::$user)) {
+			return self::$user;
+		}
+		if (self::isGuest()) {
+			return null;
+		}
+		return unserialize($_SESSION["user"]);
+	}
 
-    public static function setUser(UtilisateurRepository|null $user): void
-    {
-        if (is_null($user) || $user->archived()) {
-            Auth::logout();
-            return;
-        }
-        $_SESSION["user"] = serialize($user);
-        self::$user = $user;
-    }
+	public static function setUser(UtilisateurRepository|null $user): void
+	{
+		if (is_null($user) || $user->archived()) {
+			Auth::logout();
+			return;
+		}
+		$_SESSION["user"] = serialize($user);
+		self::$user = $user;
+	}
 
-    public static function isGuest(): bool
-    {
-        return !isset($_COOKIE["token"]) || !isset($_SESSION["user"]);
-    }
+	public static function isGuest(): bool
+	{
+		return !isset($_COOKIE["token"]) || !isset($_SESSION["user"]);
+	}
 
-    public static function getRedirect(): string
-    {
-        return "redirect=" . urlencode('http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-    }
+	public static function getRedirect(): string
+	{
+		return "redirect=" . urlencode('http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+	}
 
-    public static function redirectFromParam(string $else): void
-    {
-        if (isset($_GET["redirect"]))
-            header("Location: " . urldecode($_GET["redirect"]));
-        else
-            header("Location: " . $else);
-    }
+	public static function redirectFromParam(string $else): void
+	{
+		isset($_GET["redirect"]) ? header("Location: " . urldecode($_GET["redirect"])) : header("Location: " . $else);
+	}
 
-    public function run(): void
-    {
-        $this->triggerEvent(self::EVENT_BEFORE_REQUEST);
-        try {
-            echo $this->router->resolve();
-        } catch (\Exception $e) {
-            echo $this->router->renderView('_error', ['exception' => $e,]);
-        }
-    }
+	public function run(): void
+	{
+		$this->triggerEvent(self::EVENT_BEFORE_REQUEST);
+		try {
+			echo $this->router->resolve();
+		} catch (\Exception $e) {
+			echo $this->router->renderView('_error', ['exception' => $e,]);
+		}
+	}
 
-    public
-    function triggerEvent($eventName): void
-    {
-        $callbacks = $this->eventListeners[$eventName] ?? [];
-        foreach ($callbacks as $callback) {
-            call_user_func($callback);
-        }
-    }
+	public
+	function triggerEvent($eventName): void
+	{
+		$callbacks = $this->eventListeners[$eventName] ?? [];
+		foreach ($callbacks as $callback) {
+			call_user_func($callback);
+		}
+	}
 
-    public
-    function on($eventName, $callback): void
-    {
-        $this->eventListeners[$eventName][] = $callback;
-    }
+	public
+	function on($eventName, $callback): void
+	{
+		$this->eventListeners[$eventName][] = $callback;
+	}
 
-    public function getView(): View
-    {
-        return $this->view;
-    }
+	public function getView(): View
+	{
+		return $this->view;
+	}
 
 }
--- src/model/OffreForm.php
+++ src/model/OffreForm.php
@@ -3,6 +3,7 @@
 namespace app\src\model;
 
 use app\src\core\db\Database;
+use app\src\core\exception\ServerErrorException;
 use app\src\model\dataObject\Offre;
 use app\src\model\repository\MailRepository;
 use app\src\model\repository\StaffRepository;
@@ -10,120 +11,126 @@ use PDOException;
 
 class OffreForm extends Model
 {
-    public static function creerOffre(Offre $offre, ?string $typeStage, ?string $typeAlternance, ?float $distanciel)
-    {
-        $sql = "SELECT idoffre FROM Offre WHERE idoffre = (SELECT MAX(idoffre) FROM Offre)";
-        $stmt = Database::get_conn()->prepare($sql);
-        $stmt->execute();
-        $id = $stmt->fetch();
-        $id = $id["idoffre"];
-        $sql = "INSERT INTO Offre VALUES (:id,:dureeTag, :thematiqueTag, :sujetTag, :nbJourTravailHebdoTag, :nbHeureTravailHebdoTag, :gratificationTag, :avantageNatureTag, :dateDebutTag, :dateFinTag, :statutTag, :pourvueTag, :anneeViseeTag, :idAnneeTag, :idUtilisateurTag, :dateCreationTag, :descriptionTag)";
-        $pdoStatement = Database::get_conn()->prepare($sql);
-        $values = array(
-            "id" => $id + 1,
-            "dureeTag" => $offre->getDuree(),
-            "thematiqueTag" => $offre->getThematique(),
-            "sujetTag" => $offre->getSujet(),
-            "nbJourTravailHebdoTag" => $offre->getNbjourtravailhebdo(),
-            "nbHeureTravailHebdoTag" => $offre->getNbHeureTravailHebdo(),
-            "gratificationTag" => $offre->getGratification(),
-            "avantageNatureTag" => $offre->getAvantageNature(),
-            "dateDebutTag" => $offre->getDateDebut(),
-            "dateFinTag" => $offre->getDateFin(),
-            "statutTag" => $offre->getStatut(),
-            "pourvueTag" => $offre->getPourvue(),
-            "anneeViseeTag" => $offre->getAnneeVisee(),
-            "idAnneeTag" => $offre->getAnnee(),
-            "idUtilisateurTag" => $offre->getIdutilisateur(),
-            "dateCreationTag" => $offre->getDateCreation(),
-            "descriptionTag" => $offre->getDescription()
-        );
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function creerOffre(Offre $offre, ?string $typeStage, ?string $typeAlternance, ?float $distanciel): true
+	{
+		$sql = "SELECT idoffre FROM Offre WHERE idoffre = (SELECT MAX(idoffre) FROM Offre)";
+		$stmt = Database::get_conn()->prepare($sql);
+		$stmt->execute();
+		$id = $stmt->fetch();
+		$id = $id["idoffre"];
+		$sql = "INSERT INTO Offre VALUES (:id,:dureeTag, :thematiqueTag, :sujetTag, :nbJourTravailHebdoTag, :nbHeureTravailHebdoTag, :gratificationTag, :avantageNatureTag, :dateDebutTag, :dateFinTag, :statutTag, :pourvueTag, :anneeViseeTag, :idAnneeTag, :idUtilisateurTag, :dateCreationTag, :descriptionTag)";
+		$pdoStatement = Database::get_conn()->prepare($sql);
+		$values = array(
+			"id" => $id + 1,
+			"dureeTag" => $offre->getDuree(),
+			"thematiqueTag" => $offre->getThematique(),
+			"sujetTag" => $offre->getSujet(),
+			"nbJourTravailHebdoTag" => $offre->getNbjourtravailhebdo(),
+			"nbHeureTravailHebdoTag" => $offre->getNbHeureTravailHebdo(),
+			"gratificationTag" => $offre->getGratification(),
+			"avantageNatureTag" => $offre->getAvantageNature(),
+			"dateDebutTag" => $offre->getDateDebut(),
+			"dateFinTag" => $offre->getDateFin(),
+			"statutTag" => $offre->getStatut(),
+			"pourvueTag" => $offre->getPourvue(),
+			"anneeViseeTag" => $offre->getAnneeVisee(),
+			"idAnneeTag" => $offre->getAnnee(),
+			"idUtilisateurTag" => $offre->getIdutilisateur(),
+			"dateCreationTag" => $offre->getDateCreation(),
+			"descriptionTag" => $offre->getDescription()
+		);
 
-        $pdoStatement->execute($values);
+		$pdoStatement->execute($values);
 
-        if ($offre->getStatut() == "en attente") {
-            $emails = (new StaffRepository([]))->getManagersEmail();
-            MailRepository::send_mail($emails, "Nouvelle offre", '
+		if ($offre->getStatut() == "en attente") {
+			$emails = (new StaffRepository([]))->getManagersEmail();
+			MailRepository::send_mail($emails, "Nouvelle offre", '
  <div>
  <p>L\'entreprise ' . Application::getUser()->attributes()["nom"] . ' viens de créer une nouvelle offre</p>
  <a href="' . HOST . '/offres/' . $id . '">Voir l\'offre</a>
  </div>');
-        }
+		}
 
-        if ($typeStage != null) {
-            $sql = "INSERT INTO OffreStage VALUES (:idOffreTag)";
-            $pdoStatement = Database::get_conn()->prepare($sql);
-            $values = array(
-                "idOffreTag" => $id,
-            );
-            $pdoStatement->execute($values);
-        }
-        if ($typeAlternance != null) {
-            $sql = "INSERT INTO OffreAlternance VALUES (:idOffreTag, :distancielTag)";
-            $pdoStatement = Database::get_conn()->prepare($sql);
-            $values = array(
-                "idOffreTag" => $id,
-                "distancielTag" => $distanciel,
-            );
-            $pdoStatement->execute($values);
-        }
-        return true;
-    }
+		if ($typeStage != null) {
+			$sql = "INSERT INTO OffreStage VALUES (:idOffreTag)";
+			$pdoStatement = Database::get_conn()->prepare($sql);
+			$values = array(
+				"idOffreTag" => $id,
+			);
+			$pdoStatement->execute($values);
+		}
+		if ($typeAlternance != null) {
+			$sql = "INSERT INTO OffreAlternance VALUES (:idOffreTag, :distancielTag)";
+			$pdoStatement = Database::get_conn()->prepare($sql);
+			$values = array(
+				"idOffreTag" => $id,
+				"distancielTag" => $distanciel,
+			);
+			$pdoStatement->execute($values);
+		}
+		return true;
+	}
 
-    public static function updateOffre(Offre $offre, ?float $distanciel)
-    {
-        $sql = "UPDATE Offre SET duree=:dureeTag, thematique=:thematiqueTag, sujet=:sujetTag, nbJourTravailHebdo=:nbJourTravailHebdoTag, nbHeureTravailHebdo=:nbHeureTravailHebdoTag, gratification=:gratificationTag, uniteGratification=:unitegratificationTag, avantageNature=:avantageNatureTag, dateDebut=:dateDebutTag, dateFin=:dateFinTag, anneeVisee=:anneeViseeTag, idAnnee=:idAnneeTag, idUtilisateur=:idUtilisateurTag, description=:descriptionTag, statut=:statutTag WHERE idOffre=:idOffreTag";
-        $pdoStatement = Database::get_conn()->prepare($sql);
-        $values = array(
-            "idOffreTag" => $offre->getIdOffre(),
-            "dureeTag" => $offre->getDuree(),
-            "thematiqueTag" => $offre->getThematique(),
-            "sujetTag" => $offre->getSujet(),
-            "nbJourTravailHebdoTag" => $offre->getNbjourtravailhebdo(),
-            "nbHeureTravailHebdoTag" => $offre->getNbHeureTravailHebdo(),
-            "gratificationTag" => $offre->getGratification(),
-            "avantageNatureTag" => $offre->getAvantageNature(),
-            "dateDebutTag" => $offre->getDateDebut(),
-            "dateFinTag" => $offre->getDateFin(),
-            "anneeViseeTag" => $offre->getAnneeVisee(),
-            "idAnneeTag" => $offre->getAnnee(),
-            "idUtilisateurTag" => $offre->getIdutilisateur(),
-            "descriptionTag" => $offre->getDescription(),
-            "statutTag" => $offre->getStatut(),
-        );
-        try {
-            $pdoStatement->execute($values);
-        } catch (PDOException $e) {
-            return false;
-        }
-        if ($distanciel != null) {
-            $sql = "UPDATE OffreAlternance SET jourDistanciel=:alternanceTag WHERE idOffre=:idOffreTag";
-            $pdoStatement = Database::get_conn()->prepare($sql);
-            $values = array(
-                "idOffreTag" => $offre->getIdOffre(),
-                "alternanceTag" => $distanciel,
-            );
-            try {
-                $pdoStatement->execute($values);
-            } catch (PDOException $e) {
-                return false;
-            }
-        }
+	public static function updateOffre(Offre $offre, ?float $distanciel)
+	{
+		$sql = "UPDATE Offre SET duree=:dureeTag, thematique=:thematiqueTag, sujet=:sujetTag, nbJourTravailHebdo=:nbJourTravailHebdoTag, nbHeureTravailHebdo=:nbHeureTravailHebdoTag, gratification=:gratificationTag, uniteGratification=:unitegratificationTag, avantageNature=:avantageNatureTag, dateDebut=:dateDebutTag, dateFin=:dateFinTag, anneeVisee=:anneeViseeTag, idAnnee=:idAnneeTag, idUtilisateur=:idUtilisateurTag, description=:descriptionTag, statut=:statutTag WHERE idOffre=:idOffreTag";
+		$pdoStatement = Database::get_conn()->prepare($sql);
+		$values = array(
+			"idOffreTag" => $offre->getIdOffre(),
+			"dureeTag" => $offre->getDuree(),
+			"thematiqueTag" => $offre->getThematique(),
+			"sujetTag" => $offre->getSujet(),
+			"nbJourTravailHebdoTag" => $offre->getNbjourtravailhebdo(),
+			"nbHeureTravailHebdoTag" => $offre->getNbHeureTravailHebdo(),
+			"gratificationTag" => $offre->getGratification(),
+			"avantageNatureTag" => $offre->getAvantageNature(),
+			"dateDebutTag" => $offre->getDateDebut(),
+			"dateFinTag" => $offre->getDateFin(),
+			"anneeViseeTag" => $offre->getAnneeVisee(),
+			"idAnneeTag" => $offre->getAnnee(),
+			"idUtilisateurTag" => $offre->getIdutilisateur(),
+			"descriptionTag" => $offre->getDescription(),
+			"statutTag" => $offre->getStatut(),
+		);
+		try {
+			$pdoStatement->execute($values);
+		} catch (PDOException $e) {
+			return false;
+		}
+		if ($distanciel != null) {
+			$sql = "UPDATE OffreAlternance SET jourDistanciel=:alternanceTag WHERE idOffre=:idOffreTag";
+			$pdoStatement = Database::get_conn()->prepare($sql);
+			$values = array(
+				"idOffreTag" => $offre->getIdOffre(),
+				"alternanceTag" => $distanciel,
+			);
+			try {
+				$pdoStatement->execute($values);
+			} catch (PDOException $e) {
+				return false;
+			}
+		}
 
-    }
+	}
 
-    public static function deleteOffre(mixed $idOffre)
-    {
-        $sql = "DELETE FROM Offre WHERE idOffre=:idOffreTag";
-        $pdoStatement = Database::get_conn()->prepare($sql);
-        $values = array(
-            "idOffreTag" => $idOffre,
-        );
-        try {
-            $pdoStatement->execute($values);
-        } catch (PDOException) {
-            return false;
-        }
-        return true;
-    }
+	/**
+	 * @throws ServerErrorException
+	 */
+	public static function deleteOffre(mixed $idOffre): bool
+	{
+		$sql = "DELETE FROM Offre WHERE idOffre=:idOffreTag";
+		$pdoStatement = Database::get_conn()->prepare($sql);
+		$values = array(
+			"idOffreTag" => $idOffre,
+		);
+		try {
+			$pdoStatement->execute($values);
+		} catch (PDOException) {
+			return false;
+		}
+		return true;
+	}
 }
