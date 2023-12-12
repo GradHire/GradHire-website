9a3786b55ad41ec6f7e6c1d266c786604d7c5391
Gozzo/Giovanni <giovanni.gozzo@etu.umontpellier.fr>
2023-12-12 23:04:08 +0100

    fix: centre page import

--- src/view/import.php
+++ src/view/import.php
@@ -6,15 +6,15 @@ use app\src\model\Form\FormModel;
 
 ?>
 
-<div class="w-full max-w-md gap-4 flex flex-col pt-12 pb-24">
-	<h2 class="text-xl font-bold">Import Pstage/Studea</h2>
-	<?php $form->start(); ?>
-	<div class="w-full gap-4 flex flex-col">
-		<?php
-		$form->print_all_fields();
-		$form->submit("Importer");
-		$form->getError();
-		?>
-	</div>
-	<?php $form->end(); ?>
+<div class="w-full max-w-md gap-4 flex flex-col pt-12 pb-24 mx-auto max-w-md">
+    <h2 class="text-xl font-bold">Import Pstage/Studea</h2>
+    <?php $form->start(); ?>
+    <div class="w-full gap-4 flex flex-col">
+        <?php
+        $form->print_all_fields();
+        $form->submit("Importer");
+        $form->getError();
+        ?>
+    </div>
+    <?php $form->end(); ?>
 </div>
