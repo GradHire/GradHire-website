1a3b7cc3fb7299ac26e19d59b4b470f6a7aa5e1f
Marius Brt <marius.brouty@gmail.com>
2023-12-13 13:43:06 +0100

    Update postuler.php

--- src/view/candidature/postuler.php
+++ src/view/candidature/postuler.php
@@ -7,7 +7,8 @@ use app\src\model\Form\FormModel;
 ?>
 
 <div class="w-full max-w-md gap-4 flex flex-col gap-4 mx-auto">
-
+    <h2 class="text-3xl">Postuler</h2>
+    <span class="text-zinc-600 mb-5">Veuillez remplir les champs pour postuler à l'offre. Pensez également à contacter l'entreprise par mail ou téléphone.</span>
 	<?php $form->start(); ?>
 	<div class="w-full gap-4 flex flex-col">
 		<?php
