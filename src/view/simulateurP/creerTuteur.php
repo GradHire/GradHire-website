f1823ec91a1eba34a68ec466c0ee9794a3a82b8f
Gozzo/Giovanni <giovanni.gozzo@etu.umontpellier.fr>
2023-12-14 13:15:11 +0100

    refactor: simulation pstage

--- src/view/simulateurP/creerTuteur.php
+++ src/view/simulateurP/creerTuteur.php
@@ -10,7 +10,7 @@ use app\src\model\Form\FormModel;
 
     <h1 class="text-3xl font-bold text-center">Simulateur Pstage (Tuteur)</h1>
     <?php $form->start(); ?>
-    <div class="w-full gap-4 flex flex-col" id="step1">
+    <div class="w-full gap-4 flex flex-col">
         <?php
         $form->print_all_fields();
         $form->submit("Continuer");
