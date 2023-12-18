c2e3d5686cecdb11497e1089ecf01db93e1934da
Gozzo/Giovanni <giovanni.gozzo@etu.umontpellier.fr>
2023-12-18 12:00:08 +0100

    refactor: simulation pstage

--- src/view/import.php
+++ src/view/import.php
@@ -12,8 +12,7 @@ $this->title = 'Imports';
 ?>
 
 <div class="w-full max-w-md gap-4 flex flex-col mx-auto">
-    <h2 class="text-xl font-bold">Import Pstage/Studea</h2>
-
+    <h2 class="text-xl font-bold">Import Pstage/Studea/Scodoc</h2>
     <?php $form->start(); ?>
     <div class="w-full gap-4 flex flex-col">
         <?php
