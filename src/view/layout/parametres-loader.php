086cbc633d75733f0aaa81339e590f7d20c9a9ac
Hirchyts Daniil <daniil.hirchyts@etu.umontpellier.fr>
2023-12-19 18:04:00 +0100

    refactor: layout

--- src/view/layout/parametres-loader.php
+++ src/view/layout/parametres-loader.php
@@ -4,46 +4,88 @@
  * @var array $parametres
  */
 
+use app\src\core\exception\ServerErrorException;
+use app\src\model\Auth;
 use app\src\model\dataObject\Roles;
+
+/** @var array $allSections
+ * @var array $allActions
+ * @var array $parametres
+ */
+
+require_once __DIR__ . "/sidebar.php";
 
 $sections = ['S01'];
 $actions = [];
 
-require_once __DIR__ . "/sidebar.php";
-
-$roles = Roles::cases();
-foreach ($roles as $role) {
-    switch ($role) {
-        case Roles::Student:
-            $sections[] = 'S05';
-            $actions[] = 'A03';
-            break;
-        case Roles::Tutor:
-        case Roles::Teacher:
-            $sections[] = 'S05';
-            break;
-        case Roles::Enterprise:
-            array_push($sections, 'S01', 'S06', 'S08');
-            break;
-        case Roles::Manager:
-        case Roles::Staff:
-            array_push($sections, 'S03', 'S05', 'S06', 'S07', 'S08');
-            break;
-        default:
-            break;
-    }
+function getRoleBasedData($role, &$sections, &$actions): array
+{
+    match ($role) {
+        Roles::Student => [
+            $selectedIds = ['S01', 'S05', 'S09', 'S08'],
+            $actionsId = ['A01', 'A02', 'A03'],
+            $sections[] = 'S05',
+            $actions[] = 'A03'
+        ],
+        Roles::Tutor, Roles::Teacher => [
+            $selectedIds = ['S01', 'S05'],
+            $actionsId = ['A01', 'A02', 'A03'],
+            $sections[] = 'S05'
+        ],
+        Roles::Enterprise => [
+            $selectedIds = ['S05', 'S06'],
+            $actionsId = ['A01', 'A02', 'A03'],
+            array_push($sections, 'S01', 'S06', 'S08')
+        ],
+        Roles::Staff, Roles::Manager => [
+            $selectedIds = ['S01', 'S03', 'S05', 'S06', 'S07', 'S08'],
+            $actionsId = ['A01', 'A02', 'A04', 'A05'],
+            array_push($sections, 'S03', 'S05', 'S06', 'S07', 'S08')
+        ],
+        Roles::ChefDepartment => [
+            $selectedIds = ['S01', 'S02', 'S03', 'S04', 'S05', 'S06', 'S07', 'S08', 'S10'],
+            $actionsId = ['A01', 'A02', 'A04', 'A05'],
+            array_push($sections, 'S01', 'S02', 'S03', 'S04', 'S05', 'S06', 'S07', 'S08', 'S10')
+        ],
+        default => [
+            $selectedIds = [],
+            $actionsId = []
+        ],
+    };
+    return [$selectedIds, $actionsId];
 }
 
-$sections = array_unique($sections);
+function filterArraysByKey($selectedIds, $actionsId, &$allSections, &$allActions): void
+{
+    foreach ($allSections as $key => $value) if (!in_array($key, $selectedIds)) unset($allSections[$key]);
+    foreach ($allActions as $key => $value) if (!in_array($key, $actionsId)) unset($allActions[$key]);
+}
 
-$parametres = [
-    'sections' => $sections,
-    'actions' => $actions,
-];
+function getParametersAndDefaultData($sections, $actions): array
+{
+    $parametres = [
+        'sections' => array_unique($sections),
+        'actions' => $actions,
+    ];
+    $parametresContent = json_encode($parametres);
+    return [
+        'parametres' => $parametresContent,
+        'defaultSections' => $parametres['sections'] ?? [],
+        'defaultActions' => $parametres['actions'] ?? []
+    ];
+}
 
-$parametresContent = json_encode($parametres);
-$defaultSections = $parametres['sections'] ?? [];
-$defaultActions = $parametres['actions'] ?? [];
-$sections = $_SESSION['parametres']['sections'] ?? $defaultSections;
-$actions = $_SESSION['parametres']['actions'] ?? $defaultActions;
+try {
+    $currentRole = Auth::get_user()->role();
+} catch (ServerErrorException $e) {
+    $currentRole = Roles::Student;
+}
 
+[$selectedIds, $actionsId] = getRoleBasedData($currentRole, $sections, $actions);
+$selectedIds = array_merge($selectedIds, ['S11', 'S12']);
+
+filterArraysByKey($selectedIds, $actionsId, $allSections, $allActions);
+$data = getParametersAndDefaultData($sections, $actions);
+
+$sections = $_SESSION['parametres']['sections'] ?? $data['defaultSections'];
+$actions = $_SESSION['parametres']['actions'] ?? $data['defaultActions'];
