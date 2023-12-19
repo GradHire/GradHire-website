4424e628dfa71a2229e3dec04288d9228724f11b
Hirchyts Daniil <daniil.hirchyts@etu.umontpellier.fr>
2023-12-19 18:54:01 +0100

    refactor: layout

--- src/model/View.php
+++ src/model/View.php
@@ -2,33 +2,10 @@
 
 namespace app\src\model;
 
-use app\src\controller\AbstractController;
-
 class View
 {
-    public string $title = '';
     public static string $currentSection = '';
-
-    public function renderView($view, array $params)
-    {
-        $layoutName = Application::$app->layout;
-        if (Application::$app->controller) Application::isGuest() ? $layoutName = 'guest' : $layoutName = 'main';
-        $viewContent = $this->renderViewOnly($view, $params);
-        ob_start();
-        include_once Application::$ROOT_DIR."/src/view/layout/$layoutName.php";
-        $layoutContent = ob_get_clean();
-        return str_replace('{{content}}', $viewContent, $layoutContent);
-    }
-
-    public function renderViewOnly($view, array $params)
-    {
-        foreach ($params as $key => $value) {
-            $$key = $value;
-        }
-        ob_start();
-        include_once Application::$ROOT_DIR."/src/view/$view.php";
-        return ob_get_clean();
-    }
+    public string $title = '';
 
     public static function getCurrentSection(): string
     {
@@ -39,4 +16,27 @@ class View
     {
         self::$currentSection = $currentSection;
     }
+
+    public function renderView($view, array $params)
+    {
+        $layoutName = Application::$app->layout;
+        if (Application::$app->controller) Application::isGuest() ? $layoutName = 'guest' : $layoutName = 'main';
+        $viewContent = $this->renderViewOnly($view, $params);
+        ob_start();
+        if ($layoutName === 'main') include_once Application::$ROOT_DIR . "/src/view/layout/parametres/parametres-loader.php";
+        include_once Application::$ROOT_DIR . "/src/view/layout/$layoutName.php";
+        $layoutContent = ob_get_clean();
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    public function renderViewOnly($view, array $params)
+    {
+        foreach ($params as $key => $value) {
+            $$key = $value;
+        }
+        ob_start();
+        include_once Application::$ROOT_DIR . "/src/view/$view.php";
+        return ob_get_clean();
+    }
+
 }
