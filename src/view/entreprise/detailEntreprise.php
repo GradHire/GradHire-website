f953a14fdaa5f38ddd6c2c6739e40ce18627e038
garroc <clement.garro@etu.umontpellier.fr>
2024-01-14 19:05:35 +0100

    fix: detailEntreprise

--- src/view/entreprise/detailEntreprise.php
+++ src/view/entreprise/detailEntreprise.php
@@ -53,25 +53,18 @@ if (empty($nom) || $nom == "") $nom = "Sans nom";
     } else if (Auth::has_role(Roles::TutorTeacher, Roles::Teacher, Roles::Manager, Roles::ManagerAlternance, Roles::ManagerStage, Roles::Student)) {
         Table::button("/entreprises/" . $entreprise->getIdutilisateur() . "/posterAvis", "Poster un avis");
     }
-    /**
-     * @param $offre
-     * @return void
-     */
-    function print_cell($offre): void
-    {
-        Table::cell($offre['sujet']);
-        Table::cell($offre['thematique']);
-        Table::cell($offre['datecreation']);
-        if ($offre['statut'] == "en attente") Table::chip("En attente", "yellow");
-        else if ($offre['statut'] == "valider") Table::chip("Validée", "green");
-        else if ($offre['statut'] == "archiver") Table::chip("Archivée", "red");
-        else if ($offre['statut'] == "brouillon") Table::chip("Brouillon", "gray");
-        Table::cell("<a href=\"/offres/" . $offre['idoffre'] . "\" class=\"inline-block rounded bg-zinc-600 px-4 py-2 text-xs font-medium text-white hover:bg-zinc-700\">Voir plus</a>");
-    }
 
     if ($offres != null)
         Table::createTable($offres, ['sujet', 'thematique', 'dateCreation', 'statut'], function ($offre) {
-            print_cell($offre);
+            Table::cell($offre->getSujet());
+            Table::cell($offre->getThematique());
+            Table::cell($offre->getDatecreation());
+            $statut = $offre->getStatut();
+            if ($statut == "en attente") Table::chip("En attente", "yellow");
+            else if ($statut == "valider") Table::chip("Validée", "green");
+            else if ($statut == "archiver") Table::chip("Archivée", "red");
+            else if ($statut == "brouillon") Table::chip("Brouillon", "gray");
+            Table::cell("<a href=\"/offres/" . $offre->getIdOffre() . "\" class=\"inline-block rounded bg-zinc-600 px-4 py-2 text-xs font-medium text-white hover:bg-zinc-700\">Voir plus</a>");
         });
     Detail::end();
     ?>
