6578cc14c3eac81c415115ce8a66401c9068a7bb
Marius Brt <marius.brouty@gmail.com>
2024-01-14 16:20:11 +0100

    Update FormPassword.php

--- src/model/Form/fields/FormPassword.php
+++ src/model/Form/fields/FormPassword.php
@@ -14,9 +14,8 @@ class FormPassword extends FormString
     function field(string $name, string $value): string
     {
         $input = parent::field($name, $value);
-        $i = (time() + rand(0, 1000));
         return <<<HTML
-<div class="relative" id="password-$i">
+<div class="relative">
   $input
   <div onclick="togglePasswordVisibility(this)" class="cursor-pointer absolute right-4 top-1/2 transform -translate-y-1/2">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
@@ -26,7 +25,6 @@ class FormPassword extends FormString
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5" style="display: none">
   <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
 </svg>
-
     </div>
 </div>
 HTML;
