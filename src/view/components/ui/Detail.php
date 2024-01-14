17ea2b8c96c30bae6fb556c381198009ec14035b
garroc <clement.garro@etu.umontpellier.fr>
2024-01-14 11:39:49 +0100

    fix: detail

--- src/view/components/ui/Detail.php
+++ src/view/components/ui/Detail.php
@@ -66,7 +66,7 @@ HTML;
         echo "</dd></div>";
     }
 
-    public static function addDetail(string $title, string $content): void
+    public static function addDetail(string $title, ?string $content): void
     {
         echo "
 <div class=\"px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0\">
